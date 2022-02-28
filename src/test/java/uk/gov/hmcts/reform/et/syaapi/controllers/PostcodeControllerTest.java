81460b1e72344b426f806e086e85409e8e02601e
Brendan McHugh <brendan.mchugh@hmcts.net>
2022-02-28 17:00:48 +0000

    amend controller unit test

--- src/test/java/uk/gov/hmcts/reform/et/syaapi/controllers/PostcodeControllerTest.java
+++ src/test/java/uk/gov/hmcts/reform/et/syaapi/controllers/PostcodeControllerTest.java
@@ -1,21 +1,22 @@
 package uk.gov.hmcts.reform.et.syaapi.controllers;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.web.servlet.MockMvc;
-import uk.gov.hmcts.ecm.common.model.helper.TribunalOffice;
-import uk.gov.hmcts.reform.et.syaapi.service.PostcodeToOfficeService;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import uk.gov.hmcts.ecm.common.model.helper.TribunalOffice;
+import uk.gov.hmcts.reform.et.syaapi.service.PostcodeToOfficeService;
 
 import java.util.Optional;
 
-import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(PostcodeController.class)
 class PostcodeControllerTest {
@@ -31,13 +32,17 @@ class PostcodeControllerTest {
 
         Optional<TribunalOffice> office = Optional.of(TribunalOffice.valueOfOfficeName("Wales"));
         String postcode = "CF10 2NX";
+        String expected = "\"WALES\"";
 
         when(postcodeToOfficeService.getTribunalOfficeFromPostcode(postcode)).thenReturn(office);
-        this.mockMvc
-            .perform(get("/api/v1/postcode/")
+        MvcResult result = mockMvc
+            .perform(get("/api/v1/postcode/" + postcode)
                          .header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON))
             .andExpect(status().is(200))
-            .andExpect(jsonPath("$.size()", is(1)));
+            .andReturn();
+        String content = result.getResponse().getContentAsString();
+
+        assertEquals("correct office should be returned", content,expected);
 
     }
 
