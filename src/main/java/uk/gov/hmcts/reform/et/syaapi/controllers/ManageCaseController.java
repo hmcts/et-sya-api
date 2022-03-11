fad19ff90c461afb774f1f68d35b9b4fdab9bdc0
Marc Judge <marc.judge@HMCTS.NET>
2022-03-11 09:25:36 +0000

    Create case experiment

--- src/main/java/uk/gov/hmcts/reform/et/syaapi/controllers/ManageCaseController.java
+++ src/main/java/uk/gov/hmcts/reform/et/syaapi/controllers/ManageCaseController.java
@@ -9,9 +9,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
 import uk.gov.hmcts.reform.et.syaapi.service.CaseService;
@@ -91,6 +92,26 @@ public class ManageCaseController {
         @PathVariable @NotNull String eventType
     ) {
         var caseDetails = caseService.createCase(authorization, caseType, eventType);
+        return ok(caseDetails);
+    }
+
+    @PostMapping("/case-type/{caseType}/jurisdiction/{jurisdictionId}/event-type/{eventType}/case")
+    @Operation(summary = "Create a new default case")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Accessed successfully"),
+        @ApiResponse(responseCode = "400", description = "Bad Request"),
+        @ApiResponse(responseCode = "403", description = "Calling service is not authorised to use the endpoint"),
+        @ApiResponse(responseCode = "500", description = "Internal Server Error")
+    })
+    public ResponseEntity<CaseDetails> createCaseWithBody(
+        @RequestHeader("Authorization") String authorization,
+        @PathVariable @NotNull String caseType,
+        @PathVariable @NotNull String jurisdictionId,
+        @PathVariable @NotNull String eventType,
+        @RequestBody String caseData
+
+    ) {
+        var caseDetails = caseService.createCaseWithBody(authorization, caseType, jurisdictionId, eventType, caseData);
         return ok(caseDetails);
     }
 }
--- src/main/java/uk/gov/hmcts/reform/et/syaapi/service/CaseService.java
+++ src/main/java/uk/gov/hmcts/reform/et/syaapi/service/CaseService.java
@@ -1,5 +1,7 @@
 package uk.gov.hmcts.reform.et.syaapi.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import feign.FeignException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +12,7 @@ import uk.gov.hmcts.reform.ccd.client.model.CaseDataContent;
 import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
 import uk.gov.hmcts.reform.ccd.client.model.Event;
 import uk.gov.hmcts.reform.et.syaapi.client.CcdApiClient;
+import uk.gov.hmcts.reform.et.syaapi.models.EmploymentCaseData;
 import uk.gov.hmcts.reform.idam.client.IdamClient;
 import uk.gov.hmcts.reform.idam.client.models.UserDetails;
 
@@ -52,5 +55,42 @@ public class CaseService {
             .eventToken(ccdCase.getToken())
             .build();
         return ccdApiClient.createCase(authorization, s2sToken, caseType, caseDataContent);
+    }
+
+    @Retryable({FeignException.class, RuntimeException.class})
+    public CaseDetails createCaseWithBody(String authorization, String caseType, String jurisdictionId, String eventId,
+                                          String caseData) {
+        ObjectMapper mapper = new ObjectMapper();
+        EmploymentCaseData data = null;
+        try {
+            data = mapper.readValue(caseData, EmploymentCaseData.class);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        String s2sToken = authTokenGenerator.generate();
+        UserDetails userDetails = idamClient.getUserDetails(authorization);
+        var newCase = ccdApiClient.startForCitizen(
+            authorization,
+            s2sToken,
+            userDetails.getId(),
+            jurisdictionId,
+            caseType,
+            eventId
+        );
+
+        CaseDataContent caseDataContent = CaseDataContent.builder()
+            .event(Event.builder().id(eventId).build())
+            .eventToken(newCase.getToken())
+            .data(data)
+            .build();
+        return ccdApiClient.submitForCitizen(
+            authorization,
+            s2sToken,
+            userDetails.getId(),
+            jurisdictionId,
+            caseType,
+            true,
+            caseDataContent
+        );
     }
 }
