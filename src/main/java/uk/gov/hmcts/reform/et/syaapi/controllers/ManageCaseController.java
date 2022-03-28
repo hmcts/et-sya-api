c50a0455301c3e42109debd4e7fb8aa53c07e3e5
vinoth srinivasan <vinoth.kumarsrinivasan@HMCTS.NET>
2022-03-28 17:02:15 +0100

    ElasticSearch API search cases with latest code changes from Marc

--- src/main/java/uk/gov/hmcts/reform/et/syaapi/controllers/ManageCaseController.java
+++ src/main/java/uk/gov/hmcts/reform/et/syaapi/controllers/ManageCaseController.java
@@ -7,17 +7,18 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
+import uk.gov.hmcts.reform.et.syaapi.search.Query;
 import uk.gov.hmcts.reform.et.syaapi.service.CaseService;
+
+import org.elasticsearch.index.query.QueryBuilders;
 
 import javax.validation.constraints.NotNull;
 
+import java.util.List;
+
+import static java.util.Collections.emptyList;
 import static org.springframework.http.ResponseEntity.ok;
 
 @Slf4j
@@ -42,6 +43,35 @@ public class ManageCaseController {
     ) {
         var caseDetails = caseService.getCaseData(authorization, caseId);
         return ok(caseDetails);
+    }
+
+    /*
+    Service would talk to CCD ElasticSearch API and with one implemented method -
+    * dev shall play around DSL query to get output (couple of examples of request body as below)
+    1. Pull all cases
+        {"match_all": {}}
+    2. Pull specific cases
+        {"terms": {"reference": ["XXX", "YYY"]}}
+    3, Pull specific case
+        {"match": {"reference": "XXX"}}
+    */
+    @GetMapping("/caseTypes/{caseType}/cases")
+    @Operation(summary = "Return all case details for User")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Accessed successfully"),
+        @ApiResponse(responseCode = "400", description = "Bad Request"),
+        @ApiResponse(responseCode = "403", description = "Calling service is not authorised to use the endpoint"),
+        @ApiResponse(responseCode = "500", description = "Internal Server Error")
+    })
+    public ResponseEntity<List<CaseDetails>> getCaseDataByUser(
+        @RequestHeader("Authorization") String authorization,
+        @PathVariable String caseType,
+        @RequestBody String searchString
+    ) {
+        Query query = new Query(QueryBuilders.wrapperQuery(searchString), emptyList(), 0);
+        List<CaseDetails> casesByUser = caseService.getCaseDataByUser(authorization, caseType, query.toString());
+
+        return ok(casesByUser);
     }
 
     @PostMapping("/case-type/{caseType}/event-type/{eventType}/case")
--- src/test/java/uk/gov/hmcts/reform/et/syaapi/controllers/ManageCaseControllerTest.java
+++ src/test/java/uk/gov/hmcts/reform/et/syaapi/controllers/ManageCaseControllerTest.java
@@ -3,23 +3,32 @@ package uk.gov.hmcts.reform.et.syaapi.controllers;
 import feign.FeignException;
 import feign.Request;
 import feign.RequestTemplate;
+
+import org.elasticsearch.index.query.QueryBuilders;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+
 import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
 import uk.gov.hmcts.reform.et.syaapi.constants.EtSyaConstants;
+import uk.gov.hmcts.reform.et.syaapi.search.Query;
 import uk.gov.hmcts.reform.et.syaapi.service.CaseService;
 import uk.gov.hmcts.reform.et.syaapi.service.VerifyTokenService;
 import uk.gov.hmcts.reform.et.syaapi.utils.ResourceLoader;
 import uk.gov.hmcts.reform.et.syaapi.utils.ResourceUtil;
+import uk.gov.hmcts.reform.idam.client.IdamClient;
+import uk.gov.hmcts.reform.idam.client.models.UserDetails;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.Collections;
+import java.util.List;
+import static java.util.Collections.emptyList;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
@@ -36,6 +45,9 @@ import static uk.gov.hmcts.reform.et.syaapi.utils.TestConstants.TEST_SERVICE_AUT
 class ManageCaseControllerTest {
 
     private static final String CASE_ID = "1646225213651590";
+    private static final String USER_ID = "1234";
+    private static final String CASE_TYPE = "ET_Scotland";
+
     private final CaseDetails expectedDetails = ResourceLoader.fromString(
         "responses/caseDetails.json",
         CaseDetails.class
@@ -44,11 +56,19 @@ class ManageCaseControllerTest {
         "requests/caseData.json"
     );
 
+    private final List<CaseDetails> requestCaseDataList = ResourceLoader.fromStringToList(
+        "responses/caseDetailsList.json",
+        CaseDetails.class
+    );
+
     @Autowired
     private MockMvc mockMvc;
 
     @MockBean
     private CaseService caseService;
+
+    @MockBean
+    private IdamClient idamClient;
 
     @MockBean
     private VerifyTokenService verifyTokenService;
@@ -74,6 +94,34 @@ class ManageCaseControllerTest {
             .andExpect(jsonPath("$.state").value(expectedDetails.getState()))
             .andExpect(jsonPath("$.created_date").exists())
             .andExpect(jsonPath("$.last_modified").exists());
+    }
+
+    @Test
+    void shouldGetCaseDetailsByUser() throws Exception {
+        // given
+        String searchString = "{\"match_all\": {}}";
+        Query query = new Query(QueryBuilders.wrapperQuery(searchString), emptyList(), 0);
+
+        when(verifyTokenService.verifyTokenSignature(any())).thenReturn(true);
+        when(idamClient.getUserDetails(TEST_SERVICE_AUTH_TOKEN)).thenReturn(UserDetails.builder().id(USER_ID).build());
+        when(caseService.getCaseDataByUser(
+            TEST_SERVICE_AUTH_TOKEN,
+            CASE_TYPE, query.toString()
+        ))
+            .thenReturn(requestCaseDataList);
+
+        // when
+        mockMvc.perform(
+                get("/caseTypes/{caseType}/cases", CASE_TYPE).contentType(MediaType.APPLICATION_JSON).content(searchString)
+                    .header(HttpHeaders.AUTHORIZATION, TEST_SERVICE_AUTH_TOKEN))
+            // then
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("[0].case_type_id").value(requestCaseDataList.get(0).getCaseTypeId()))
+            .andExpect(jsonPath("[0].jurisdiction").value(requestCaseDataList.get(0).getJurisdiction()))
+            .andExpect(jsonPath("[0].state").value(requestCaseDataList.get(0).getState()))
+            .andExpect(jsonPath("[0].created_date").exists())
+            .andExpect(jsonPath("[0].last_modified").exists())
+            .andExpect(jsonPath("[1].case_type_id").value(requestCaseDataList.get(1).getCaseTypeId()));
     }
 
     @Test
