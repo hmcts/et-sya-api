80248a622afdb48cf87d4daa47b24b98857ecb52
vinoth srinivasan <vinoth.kumarsrinivasan@HMCTS.NET>
2022-03-30 12:34:53 +0100

    code review comments for elastic search changes incorporated 30-MAR-22

--- src/main/java/uk/gov/hmcts/reform/et/syaapi/service/CaseService.java
+++ src/main/java/uk/gov/hmcts/reform/et/syaapi/service/CaseService.java
@@ -11,7 +11,6 @@ import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGenerator;
 import uk.gov.hmcts.reform.ccd.client.model.CaseDataContent;
 import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
 import uk.gov.hmcts.reform.ccd.client.model.Event;
-import uk.gov.hmcts.reform.ccd.client.model.SearchResult;
 import uk.gov.hmcts.reform.et.syaapi.client.CcdApiClient;
 import uk.gov.hmcts.reform.et.syaapi.models.EmploymentCaseData;
 import uk.gov.hmcts.reform.idam.client.IdamClient;
