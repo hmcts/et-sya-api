8a0bf83ccce3dc60676e95f514cda43c03c79d56
vinoth srinivasan <vinoth.kumarsrinivasan@HMCTS.NET>
2022-04-20 14:54:57 +0100

    Updated test cases for sending email

--- src/main/java/uk/gov/hmcts/reform/et/syaapi/config/notification/NotificationsProperties.java
+++ src/main/java/uk/gov/hmcts/reform/et/syaapi/config/notification/NotificationsProperties.java
@@ -1,6 +1,8 @@
 package uk.gov.hmcts.reform.et.syaapi.config.notification;
 
 import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.validation.annotation.Validated;
 
 import javax.validation.constraints.NotEmpty;
@@ -8,9 +10,10 @@ import javax.validation.constraints.NotEmpty;
 @Validated
 @Data
 public class NotificationsProperties {
-    @NotEmpty
+
+    @Value("${notifications.govNotifyApiKey}")
     private String govNotifyApiKey;
 
-    @NotEmpty
-    private String sampleEmailTemplate;
+    @Value("${notifications.sampleEmailTemplateId}")
+    private String sampleEmailTemplateId;
 }
--- src/test/java/uk/gov/hmcts/reform/et/syaapi/service/NotificationServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/et/syaapi/service/NotificationServiceTest.java
@@ -1,16 +1,9 @@
 package uk.gov.hmcts.reform.et.syaapi.service;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import uk.gov.hmcts.ecm.common.model.ccd.Et1CaseData;
-import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
-import uk.gov.hmcts.reform.ccd.client.model.StartEventResponse;
 import uk.gov.hmcts.reform.et.syaapi.config.notification.NotificationsProperties;
-import uk.gov.hmcts.reform.et.syaapi.helper.CaseDetailsConverter;
-import uk.gov.hmcts.reform.et.syaapi.utils.ResourceLoader;
 import uk.gov.service.notify.NotificationClient;
 import uk.gov.service.notify.SendEmailResponse;
 
@@ -21,6 +14,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class NotificationServiceTest {
 
+    private final String GOV_NOTIFY_API_KEY = "et_test_api_key-002d2170-e381-4545-8251-5e87dab724e7-190d8b02-2bb8-4fc9-a471-5486b77782c0";
+    private final String SAMPLE_TEMPLATE_API_KEY = "8835039a-3544-439b-a3da-882490d959eb";
+
     private NotificationService notificationService;
     private NotificationClient notificationClient;
 
@@ -29,10 +25,8 @@ class NotificationServiceTest {
 
     @BeforeEach
     void setUp() {
-        notificationClient = new NotificationClient(
-            notificationsProperties.getGovNotifyApiKey()
-           // "et_test_api_key-002d2170-e381-4545-8251-5e87dab724e7-190d8b02-2bb8-4fc9-a471-5486b77782c0");
-        );
+        notificationsProperties = new NotificationsProperties();
+        notificationClient = new NotificationClient(GOV_NOTIFY_API_KEY);
         notificationService = new NotificationService(notificationClient);
     }
 
@@ -57,7 +51,7 @@ class NotificationServiceTest {
         parameters.put("firstname", "Vinothkumar");
         String reference = "TEST EMAIL Alert";
         SendEmailResponse sendEmailResponse = notificationService.sendMail(
-            notificationsProperties.getSampleEmailTemplate(), targetEmail, parameters, reference);
+            SAMPLE_TEMPLATE_API_KEY, targetEmail, parameters, reference);
         assertThat(sendEmailResponse.getReference().get()).isEqualTo(reference);
     }
 }
