c03bbb7cd1c7f73620ea20dce5e42320fef9cf3c
lvo700 <lynnette.orr@HMCTS.net>
2022-04-21 16:44:50 +0100

    Added claim case and docmosis models

---
+++ src/main/java/uk/gov/hmcts/reform/et/syaapi/models/ClaimCaseDocument.java
@@ -0,0 +1,12 @@
+package uk.gov.hmcts.reform.et.syaapi.models;
+
+import lombok.Data;
+
+/**
+ * This is a {@link DocmosisDocument} to hold all required data to send to Docmosis (Tornado) for the purpose of
+ * generating a Claim Case PDF document as populated during the citizen journey of the ET1 application form.
+ */
+@Data
+public class ClaimCaseDocument implements DocmosisDocument {
+
+}
