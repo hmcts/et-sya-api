d6b1fb9b3a8a4af7418d49a983c4e35af3eafa56
vinoth srinivasan <vinoth.kumarsrinivasan@HMCTS.NET>
2022-04-27 13:44:23 +0100

    Adding enum for testing the build

---
+++ src/main/java/uk/gov/hmcts/reform/et/syaapi/enums/CaseModel.java
@@ -0,0 +1,10 @@
+package uk.gov.hmcts.reform.et.syaapi.enums;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum CaseModel {
+    SUBMIT_CASE_DRAFT
+}
