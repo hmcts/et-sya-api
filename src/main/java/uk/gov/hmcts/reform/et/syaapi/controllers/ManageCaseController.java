aa05ceb3564a4603eabf652a8bf6008a81e799de
Brendan McHugh <brendan.mchugh@hmcts.net>
2022-05-10 11:18:44 +0100

    remove unused imports

--- src/main/java/uk/gov/hmcts/reform/et/syaapi/controllers/ManageCaseController.java
+++ src/main/java/uk/gov/hmcts/reform/et/syaapi/controllers/ManageCaseController.java
@@ -4,9 +4,6 @@ import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.InputStreamResource;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,7 +18,6 @@ import uk.gov.hmcts.reform.et.syaapi.annotation.ApiResponseGroup;
 import uk.gov.hmcts.reform.et.syaapi.enums.CaseEvent;
 import uk.gov.hmcts.reform.et.syaapi.service.CaseService;
 
-import java.io.IOException;
 import java.util.List;
 import javax.validation.constraints.NotNull;
 
@@ -83,5 +79,4 @@ public class ManageCaseController {
     ) {
         return caseService.triggerEvent(authorization, caseId, caseType, CaseEvent.valueOf(eventType), caseData);
     }
-
 }
