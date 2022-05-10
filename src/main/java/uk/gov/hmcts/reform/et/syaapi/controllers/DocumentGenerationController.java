c1e7556d0150e85fe08315495e80f83e53a04c0b
Brendan McHugh <brendan.mchugh@hmcts.net>
2022-05-10 14:35:06 +0100

    tidy up controller response

--- src/main/java/uk/gov/hmcts/reform/et/syaapi/controllers/DocumentGenerationController.java
+++ src/main/java/uk/gov/hmcts/reform/et/syaapi/controllers/DocumentGenerationController.java
@@ -3,12 +3,14 @@ package uk.gov.hmcts.reform.et.syaapi.controllers;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
+//import org.springframework.http.HttpHeaders;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import uk.gov.hmcts.reform.et.syaapi.annotation.ApiResponseGroup;
+import uk.gov.hmcts.reform.et.syaapi.models.TornadoDocument;
+import uk.gov.hmcts.reform.et.syaapi.models.ClaimCaseDocument;
 
 import java.io.IOException;
 
@@ -19,26 +21,20 @@ import static uk.gov.hmcts.reform.et.syaapi.constants.EtSyaConstants.AUTHORIZATI
 @RestController
 public class DocumentGenerationController {
 
-    @PostMapping(value = "/generate-case-pdf", produces = "application/pdf")
+    @PostMapping(value = "/generatePDF", produces = "application/pdf")
     @Operation(summary = "Generate submitted case pdf")
     @ApiResponseGroup
     public ResponseEntity<byte[]> generatePdf(
         @RequestHeader(AUTHORIZATION) String authorization,
         @RequestBody String caseId
     ) throws IOException {
-
-        byte[] contents = pdfStub();
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_PDF);
-        String fileName = "submitted-case.pdf";
-        headers.setContentDispositionFormData("attachment", fileName);
-        headers.setCacheControl("must-revalidate, post-check=0, pre-check=0");
-        ResponseEntity<byte[]> response = new ResponseEntity<>(contents, headers, HttpStatus.OK);
-        return response;
+        ClaimCaseDocument tornadoDoc = new ClaimCaseDocument();
+        byte[] pdfDocument = genPdfDocumentStub("add-template-name", "outputFileName.pdf", tornadoDoc);
+        log.info("Generated document");
+        return ResponseEntity.ok(pdfDocument);
     }
 
-    private byte[] pdfStub() throws IOException {
-        byte[] helloWorldPdf = this.getClass().getClassLoader().getResourceAsStream("HelloWorld.pdf").readAllBytes();
-        return helloWorldPdf;
+    private byte[] genPdfDocumentStub(String templateName, String outputFileName, TornadoDocument sourceData) throws IOException {
+        return this.getClass().getClassLoader().getResourceAsStream("HelloWorld.pdf").readAllBytes();
     }
 }
