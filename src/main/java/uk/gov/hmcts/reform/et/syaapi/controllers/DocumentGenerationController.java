c7e47866c8e185fd90f87fdc35f1dc0285c05eca
Brendan McHugh <brendan.mchugh@hmcts.net>
2022-05-11 15:08:11 +0100

    pmd and checkstyle amendments

--- src/main/java/uk/gov/hmcts/reform/et/syaapi/controllers/DocumentGenerationController.java
+++ src/main/java/uk/gov/hmcts/reform/et/syaapi/controllers/DocumentGenerationController.java
@@ -4,10 +4,13 @@ import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RestController;
 import uk.gov.hmcts.reform.et.syaapi.annotation.ApiResponseGroup;
-import uk.gov.hmcts.reform.et.syaapi.models.TornadoDocument;
 import uk.gov.hmcts.reform.et.syaapi.models.ClaimCaseDocument;
+import uk.gov.hmcts.reform.et.syaapi.models.TornadoDocument;
 
 import java.io.IOException;
 
@@ -26,12 +29,23 @@ public class DocumentGenerationController {
         @RequestBody String caseId
     ) throws IOException {
         ClaimCaseDocument tornadoDoc = new ClaimCaseDocument();
-        byte[] pdfDocument = genPdfDocumentStub("add-template-name", "outputFileName.pdf", tornadoDoc);
+        byte[] pdfDocument =
+            genPdfDocumentStub("add-template-name", "outputFileName.pdf", tornadoDoc);
         log.info("Generated document");
         return ResponseEntity.ok(pdfDocument);
     }
 
-    private byte[] genPdfDocumentStub(String templateName, String outputFileName, TornadoDocument sourceData) throws IOException {
-        return this.getClass().getClassLoader().getResourceAsStream("HelloWorld.pdf").readAllBytes();
+    /**
+     * Stub returns a HelloWorld.pdf byte array.
+     * @param templateName The name of the template
+     * @param outputFileName filename for the generated pdf
+     * @param sourceData The case data being used
+     * @return byte[] pdf binary
+     * @throws IOException if there is an issue reading the pdf from the sorurce folder
+     */
+    protected byte[] genPdfDocumentStub(String templateName,
+                                      String outputFileName,
+                                      TornadoDocument sourceData) throws IOException {
+        return Thread.currentThread().getContextClassLoader().getResourceAsStream("HelloWorld.pdf").readAllBytes();
     }
 }
--- src/test/java/uk/gov/hmcts/reform/et/syaapi/controllers/DocumentGenerationControllerTest.java
+++ src/test/java/uk/gov/hmcts/reform/et/syaapi/controllers/DocumentGenerationControllerTest.java
@@ -13,14 +13,14 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import uk.gov.hmcts.reform.et.syaapi.service.VerifyTokenService;
 import uk.gov.hmcts.reform.et.syaapi.utils.ResourceUtil;
-import uk.gov.hmcts.reform.idam.client.IdamClient;
 
 import java.io.IOException;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static uk.gov.hmcts.reform.et.syaapi.utils.TestConstants.TEST_SERVICE_AUTH_TOKEN;
 
 @WebMvcTest(
@@ -29,17 +29,13 @@ import static uk.gov.hmcts.reform.et.syaapi.utils.TestConstants.TEST_SERVICE_AUT
 @Import(DocumentGenerationController.class)
 
 class DocumentGenerationControllerTest {
-    private static final String CASE_ID = "12345678";
-    private static final String USER_ID = "1234";
+
     private final String requestJson = ResourceUtil.resourceAsString(
         "requests/caseId.json"
     );
 
     @Autowired
     private MockMvc mockMvc;
-
-    @MockBean
-    private IdamClient idamClient;
 
     @MockBean
     private VerifyTokenService verifyTokenService;
@@ -50,10 +46,11 @@ class DocumentGenerationControllerTest {
 
     @SneakyThrows
     @Test
-    void shouldReturnPdfAsByteArray(){
+    void shouldReturnPdfAsByteArray() {
         when(verifyTokenService.verifyTokenSignature(any())).thenReturn(true);
 
-        byte[] expectedResult = this.getClass().getClassLoader().getResourceAsStream("HelloWorld.pdf").readAllBytes();
+        byte[] expectedResult =
+            Thread.currentThread().getContextClassLoader().getResourceAsStream("HelloWorld.pdf").readAllBytes();
 
         MvcResult result = mockMvc.perform(post("/generatePDF")
                             .header(HttpHeaders.AUTHORIZATION, TEST_SERVICE_AUTH_TOKEN)
@@ -64,7 +61,7 @@ class DocumentGenerationControllerTest {
             .andReturn();
 
         byte[] receivedContent = result.getResponse().getContentAsByteArray();
-        Assert.assertArrayEquals(receivedContent, expectedResult );
+        Assert.assertArrayEquals(receivedContent, expectedResult);
 
     }
 
