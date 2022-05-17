b3908ba6b1d11e3b3fca101104d756bf54ad9f7a
vinothon84 <vinoth.kumarsrinivasan@HMCTS.NET>
2022-05-17 13:50:32 +0100

    Pdg generation PACT testing API

---
+++ src/contractTest/java/uk/gov/hmcts/reform/et/syaapi/consumer/document/PdfGenerationServiceConsumerTest.java
@@ -0,0 +1,110 @@
+package uk.gov.hmcts.reform.et.syaapi.consumer.document;
+
+import au.com.dius.pact.consumer.MockServer;
+import au.com.dius.pact.consumer.dsl.PactDslWithProvider;
+import au.com.dius.pact.consumer.junit5.PactConsumerTestExt;
+import au.com.dius.pact.consumer.junit5.PactTestFor;
+import au.com.dius.pact.core.model.RequestResponsePact;
+import au.com.dius.pact.core.model.annotations.Pact;
+import au.com.dius.pact.core.model.annotations.PactFolder;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.fluent.Executor;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.json.JSONException;
+import org.junit.After;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import uk.gov.hmcts.reform.et.syaapi.models.ClaimCaseDocument;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+
+@ExtendWith(PactConsumerTestExt.class)
+@ExtendWith(SpringExtension.class)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@PactTestFor(providerName = "rpePdfService_PDFGenerationEndpointV2", port = "8891")
+@PactFolder("pacts")
+@SpringBootTest({
+    "service.pdf-service.uri : http://localhost:8891/pdfs"
+})
+class PdfGenerationServiceConsumerTest {
+    private static final String SERVICE_AUTHORIZATION_HEADER = "ServiceAuthorization";
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    private final String someServiceAuthToken = "someServiceAuthToken";
+    private Map placeholders = Map.of("caseNo", "12345");
+
+    @BeforeEach
+    void setUpEachTest() throws InterruptedException, IOException {
+        Thread.sleep(2000);
+    }
+
+    @After
+    void teardown() {
+        Executor.closeIdleConnections();
+    }
+
+    @Pact(provider = "rpePdfService_PDFGenerationEndpointV2", consumer = "fprl_documentGeneratorClient")
+    RequestResponsePact generatePdfFromTemplate(PactDslWithProvider builder) throws JSONException, IOException {
+        return builder
+            .given("A request to generate a pdf document")
+            .uponReceiving("a request to generate a pdf document with a template")
+            .method("POST")
+            .headers(SERVICE_AUTHORIZATION_HEADER, someServiceAuthToken)
+            .body(createJsonObject(createClaimCase()),
+                  "application/vnd.uk.gov.hmcts.pdf-service.v2+json;charset=UTF-8")
+            .path("/pdfs")
+            .willRespondWith()
+            .withBinaryData("".getBytes(), "application/octet-stream")
+            .matchHeader(org.springframework.http.HttpHeaders.CONTENT_TYPE,
+                         "application/pdf")
+            .status(200)
+            .toPact();
+    }
+
+    @Test
+    @PactTestFor(pactMethod = "generatePdfFromTemplate")
+    public void verifyGeneratePdfFromTemplatePact(MockServer mockServer) throws IOException, JSONException {
+
+        HttpClient httpClient = HttpClientBuilder.create().build();
+
+        HttpPost request = new HttpPost(mockServer.getUrl() + "/pdfs");
+        StringEntity json = new StringEntity(createJsonObject(createClaimCase()));
+        request.addHeader(SERVICE_AUTHORIZATION_HEADER, someServiceAuthToken);
+        request.addHeader("content-type", "application/vnd.uk.gov.hmcts.pdf-service.v2+json;charset=UTF-8");
+        request.setEntity(json);
+
+        HttpResponse generateDocumentResponse = httpClient.execute(request);
+
+        String responseContentType = generateDocumentResponse.getEntity().getContentType().toString();
+
+        assertEquals(200, generateDocumentResponse.getStatusLine().getStatusCode());
+        assertEquals("Content-Type: application/pdf", responseContentType);
+    }
+
+
+    protected String createJsonObject(Object obj) throws JSONException, IOException {
+        return objectMapper.writeValueAsString(obj);
+    }
+
+    private ClaimCaseDocument createClaimCase() {
+        ClaimCaseDocument claimCaseDocument = new ClaimCaseDocument();
+        claimCaseDocument.setTestMessage("Hello World");
+        return claimCaseDocument;
+    }
+}
