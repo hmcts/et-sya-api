ccee3fe32d88f4938106f36be45032afeb49e943
Vino Srinivasan <35428275+vinothon84@users.noreply.github.com>
2022-05-17 16:22:37 +0100

    Ret 1452 latest (#93)
    * Initial code changes for PACT testing
    
    * Contract code changes with getCases execution
    
    * Fix version for running pact tests
    
    * Fix version for running pact tests
    
    * Remove unused import to fix jenkijs pipeline
    
    * Changes to jenkins to include contract testing
    
    * Changes to jenkins to include contract testing
    
    * Fix jenkins pipeline
    
    * Fix jenkins pipeline
    
    * Fix jenkins pipeline
    
    * fixing missing import
    
    * Code changes for Start cae and start event working
    
    * Updated pact method for submitForCitizen
    
    * Working version of ccd event code changes
    
    * Code changes for S2S pact tests
    
    * Fix version for S2S pact testing
    
    * Fix after initial code review
    
    * Fix after initial code review for StartEventForCitizenConsumerTest
    
    * json fixes for submit and submit event for Citizen
    
    * PACT changes for Idam api
    
    * pmd code review fixes
    
    * Pmd code review incorporation
    
    * Fix for pushing json output to pacts directory
    
    * Remove unused imports to pass jenkins build
    
    * Pdg generation PACT testing API
    
    * Code review comments provided by peers and pdf generation PACT build changes
    
    Co-authored-by: vinothon84 <vinoth.kumarsrinivasan@HMCTS.NET>
    Co-authored-by: satya <1426759+satyachundur@users.noreply.github.com>

---
+++ src/contractTest/java/uk/gov/hmcts/reform/et/syaapi/consumer/SpringBootContractBaseTest.java
@@ -0,0 +1,88 @@
+package uk.gov.hmcts.reform.et.syaapi.consumer;
+
+import au.com.dius.pact.consumer.dsl.DslPart;
+import au.com.dius.pact.consumer.junit5.PactConsumerTestExt;
+import au.com.dius.pact.consumer.junit5.PactTestFor;
+import au.com.dius.pact.core.model.annotations.PactFolder;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import uk.gov.hmcts.reform.ccd.client.CoreCaseDataApi;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import static io.pactfoundation.consumer.dsl.LambdaDsl.newJsonBody;
+
+@Slf4j
+@ExtendWith(PactConsumerTestExt.class)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@PactTestFor(providerName = "ccd_data_store_cases_api", port = "8890")
+@PactFolder("pacts")
+@SpringBootTest({
+    "core_case_data.api.url : localhost:8890"
+})
+public class SpringBootContractBaseTest {
+    public static final String AUTH_TOKEN = "Bearer someAuthorizationToken";
+    public static final String SERVICE_AUTH_TOKEN = "Bearer someServiceAuthorizationToken";
+
+    public static final String INITIATE_CASE_DRAFT = "INITIATE_CASE_DRAFT";
+    public static final String UPDATE_CASE_DRAFT = "UPDATE_CASE_DRAFT";
+    protected static final String USER_ID = "123456";
+    protected static final Long CASE_ID = 1_593_694_526_480_033L;
+    public static final String JURISDICTION = "jurisdictionId";
+    public static final String CASE_TYPE = "caseType";
+    public static final String PUBLIC = "PUBLIC";
+    public static final String JURISDICTION_ID = "EMPLOYMENT";
+    public static final String CASE_TYPE_ID = "ET_EnglandWales";
+    public static final String EVENT_ID = "EVENT_ID";
+    public static final int SLEEP_TIME = 2000;
+    protected static final String ALPHABETIC_REGEX = "[/^[A-Za-z_]+$/]+";
+    @Autowired
+    protected CoreCaseDataApi coreCaseDataApi;
+    public static final Map<String, String> RESPONSE_HEADERS = Map.of("Content-Type", "application/json");
+
+    @BeforeEach
+    public void prepareTest() throws Exception {
+        Thread.sleep(SLEEP_TIME);
+    }
+
+    protected Map<String, Object> addCaseTypeJurdisticaton() {
+        Map<String, Object> map = new ConcurrentHashMap<>();
+        map.put(JURISDICTION, JURISDICTION_ID);
+        map.put(CASE_TYPE, CASE_TYPE_ID);
+        return map;
+    }
+
+    protected static DslPart buildStartEventResponseWithEmptyCaseDetails(String eventId) {
+        return newJsonBody((o) -> {
+            o.stringType("event_id", eventId)
+                .stringType("token", null)
+                .object("case_details", (cd) -> {
+                    cd.numberType("id", CASE_ID);
+                    cd.stringMatcher("jurisdiction", ALPHABETIC_REGEX, "EMPLOYMENT");
+                    cd.stringType("callback_response_status", null);
+                    cd.stringMatcher("case_type_id", ALPHABETIC_REGEX, "ET_EnglandWales");
+                    cd.object("case_data", data -> {
+                    });
+                });
+        }).build();
+    }
+
+    public static DslPart buildCaseDetailsDsl(Long caseId) {
+        return newJsonBody((o) -> {
+            o.numberType("id", caseId)
+                .stringType("jurisdiction", "EMPLOYMENT")
+                .stringType("state", "ADMISSION_TO_HMCTS")
+                .stringValue("case_type_id", "ET_EnglandWales")
+                .object("case_data", (dataMap) -> {
+                    dataMap
+                        .stringType("caseType", "Single")
+                        .stringType("caseSource", "Manually Created");
+                });
+        }).build();
+    }
+}
---
+++ src/contractTest/java/uk/gov/hmcts/reform/et/syaapi/consumer/idam/JwksConsumerTest.java
@@ -0,0 +1,78 @@
+package uk.gov.hmcts.reform.et.syaapi.consumer.idam;
+
+import au.com.dius.pact.consumer.MockServer;
+import au.com.dius.pact.consumer.dsl.PactDslJsonBody;
+import au.com.dius.pact.consumer.dsl.PactDslWithProvider;
+import au.com.dius.pact.consumer.junit5.PactConsumerTestExt;
+import au.com.dius.pact.consumer.junit5.PactTestFor;
+import au.com.dius.pact.core.model.RequestResponsePact;
+import au.com.dius.pact.core.model.annotations.Pact;
+import au.com.dius.pact.core.model.annotations.PactFolder;
+import io.restassured.RestAssured;
+import org.assertj.core.api.Assertions;
+import org.json.JSONObject;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.Map;
+
+import static org.springframework.http.HttpMethod.GET;
+import static org.springframework.http.HttpStatus.OK;
+
+@ExtendWith(PactConsumerTestExt.class)
+@ExtendWith(SpringExtension.class)
+@PactFolder("pacts")
+class JwksConsumerTest {
+    private static final String JWKS_AUTH_URL = "/o/jwks";
+
+    @Pact(provider = "idam_jwks_api", consumer = "et_sya_api_service")
+    RequestResponsePact executeServiceAuthApiGetToke(PactDslWithProvider builder) {
+
+        Map<String, String> responseHeaders = Map.of(HttpHeaders.AUTHORIZATION, "Bearer UserAuthToken");
+
+        return builder
+            .given("a case exists")
+            .uponReceiving("Provider receives a token request request from et-sya-api API")
+            .path(JWKS_AUTH_URL)
+            .method(GET.toString())
+            .willRespondWith()
+            .status(OK.value())
+            .headers(responseHeaders)
+            .body(createAuthResponse())
+            .toPact();
+    }
+
+    @Test
+    @PactTestFor(pactMethod = "executeServiceAuthApiGetToke")
+    void shouldReceiveTokenAnd200(MockServer mockServer) {
+
+        String responseBody = RestAssured
+            .given()
+            .contentType(MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+            .log().all(true)
+            .when()
+            .get(mockServer.getUrl() + JWKS_AUTH_URL)
+            .then()
+            .statusCode(200)
+            .and()
+            .extract()
+            .asString();
+
+        JSONObject response = new JSONObject(responseBody);
+        Assertions.assertThat(response).isNotNull();
+
+    }
+
+    private PactDslJsonBody createAuthResponse() {
+        return new PactDslJsonBody()
+            .eachLike("keys")
+                .stringType("kid", "KeyId1")
+                .stringType("kty", "RSA")
+                .stringType("e", "AQAB")
+                .stringType("use", "Public Key Use1")
+                .stringType("n", "someToken");
+    }
+}
