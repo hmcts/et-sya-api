37a86d5a147097140187cb56c9108259c5e6d0f2
Peter Moores <peter.moores@version1.com>
2022-05-26 17:04:43 +0100

    Fixed check style error.

--- src/test/java/uk/gov/hmcts/reform/et/syaapi/service/NotificationServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/et/syaapi/service/NotificationServiceTest.java
@@ -15,7 +15,9 @@ import java.util.concurrent.ConcurrentHashMap;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.nullable;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
