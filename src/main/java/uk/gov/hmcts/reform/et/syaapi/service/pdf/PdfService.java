94e67a7468f3e6c02959b30c0f0f88a7f07de1df
Daryl Boam <daryl.boam@version1.com>
2022-07-22 16:35:19 +0100

    2011: write impl and test cases for pdf service

--- src/main/java/uk/gov/hmcts/reform/et/syaapi/service/pdf/PdfService.java
+++ src/main/java/uk/gov/hmcts/reform/et/syaapi/service/pdf/PdfService.java
@@ -1,27 +1,53 @@
 package uk.gov.hmcts.reform.et.syaapi.service.pdf;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+import lombok.RequiredArgsConstructor;
+import org.apache.pdfbox.Loader;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDDocumentCatalog;
+import org.apache.pdfbox.pdmodel.interactive.form.PDAcroForm;
+import org.apache.pdfbox.pdmodel.interactive.form.PDField;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.util.ResourceUtils;
 import uk.gov.hmcts.et.common.model.ccd.CaseData;
 
 /*
  * Prints out provided case as a PDF.
  */
 @Service
+@RequiredArgsConstructor()
 public class PdfService {
 
     private final PdfMapperService pdfMapperService;
-
-    private final String pdfTemplateSource;
-
-    public PdfService(PdfMapperService pdfMapperService,
-                      @Value("${pdf.source}") String pdfTemplateSource) {
-        this.pdfMapperService = pdfMapperService;
-        this.pdfTemplateSource = pdfTemplateSource;
-    }
+    @Value("${pdf.source}")
+    private String pdfTemplateSource;
 
     public byte[] convertCaseToPdf(CaseData caseData) {
-        return "".getBytes();
+        byte[] generatedPDF = {};
+        try (PDDocument pdfDocument = Loader.loadPDF(ResourceUtils.getFile(this.pdfTemplateSource))) {
+            PDDocumentCatalog pdDocumentCatalog = pdfDocument.getDocumentCatalog();
+            PDAcroForm pdfForm = pdDocumentCatalog.getAcroForm();
+            this.pdfMapperService.mapHeadersToPdf(caseData).forEach((k, v) -> {
+                PDField pdfField = pdfForm.getField(k);
+                try {
+                    pdfField.setValue(v.get());
+                } catch (Exception e) {
+                    //continue if field mapping error
+                    e.printStackTrace();
+                    return;
+                }
+            });
+            pdfForm.flatten();
+            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+            pdfDocument.save(byteArrayOutputStream);
+            pdfDocument.close();
+            return byteArrayOutputStream.toByteArray();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        return generatedPDF;
     }
-
 }
