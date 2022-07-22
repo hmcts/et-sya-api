755d555f1f1f96b557b72408c32cd2ef85a3d050
Daryl Boam <daryl.boam@version1.com>
2022-07-22 16:34:10 +0100

    2011: write impl and test cases for pdf service

--- src/test/java/uk/gov/hmcts/reform/et/syaapi/service/pdf/PdfServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/et/syaapi/service/pdf/PdfServiceTest.java
@@ -1,31 +1,109 @@
 package uk.gov.hmcts.reform.et.syaapi.service.pdf;
 
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+import static java.util.Optional.ofNullable;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+import org.apache.pdfbox.Loader;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDDocumentCatalog;
+import org.apache.pdfbox.pdmodel.interactive.form.PDAcroForm;
+import org.apache.pdfbox.pdmodel.interactive.form.PDField;
+import org.apache.pdfbox.pdmodel.interactive.form.PDNonTerminalField;
+import org.elasticsearch.core.Tuple;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
+import org.springframework.test.util.ReflectionTestUtils;
+import org.springframework.util.ResourceUtils;
 import uk.gov.hmcts.et.common.model.ccd.CaseData;
 
 @SuppressWarnings({"PMD.TooManyMethods"})
+@ExtendWith(MockitoExtension.class)
+@MockitoSettings(strictness = Strictness.LENIENT)
 public class PdfServiceTest {
 
-    private PdfService pdfService;
+    private static final Map<String, Optional<String>> PDF_VALUES = Map.of(
+        PdfMapperConstants.TRIBUNAL_OFFICE, ofNullable("Manchester"),
+        PdfMapperConstants.CASE_NUMBER, ofNullable("001"),
+        PdfMapperConstants.DATE_RECEIVED, ofNullable("21-07-2022"),
+        PdfMapperConstants.Q1_FIRST_NAME, ofNullable("TEST NAME"),
+        PdfMapperConstants.Q1_SURNAME, ofNullable("TEST SURNAME")
+    );
 
-    private CaseData caseData = new CaseData();
+    @Mock
+    private CaseData caseData;
 
     @Mock
     private PdfMapperService pdfMapperService;
 
+    @InjectMocks
+    private PdfService pdfService;
+
     @BeforeEach
     void setup() {
-
-
-        String pdfSourceUrl = "classpath:ET1_0722.pdf";
-
-        pdfService = new PdfService(pdfMapperService, pdfSourceUrl);
-
+        ReflectionTestUtils.setField(pdfService, "pdfTemplateSource", "classpath:ET1_0722_mod.pdf");
     }
 
-    // TODO - create MOCK return values
-    // TODO - test that PDF is created
-    // TODO - Add a helper to read the pdf variables
-    // TODO - use reader to check that attributes match
+    @Test
+    void givenPdfValuesProducesAPdfDocument() throws IOException {
+        when(pdfMapperService.mapHeadersToPdf(caseData)).thenReturn(PDF_VALUES);
+        byte[] pdfBytes = pdfService.convertCaseToPdf(caseData);
+
+        assertThat(pdfBytes).isNotNull();
+    }
+
+    @Test
+    void givenPdf() throws IOException{
+        when(pdfMapperService.mapHeadersToPdf(caseData)).thenReturn(PDF_VALUES);
+        byte[] pdfBytes = pdfService.convertCaseToPdf(caseData);
+
+        try (PDDocument templatePdf = Loader.loadPDF(ResourceUtils.getFile("classpath:ET1_0722_mod.pdf"))) {
+            Map<String, Optional<String>> templatePdfValues = processPdf(templatePdf);
+
+            try(PDDocument actualPdf = Loader.loadPDF(pdfBytes)) {
+                Map<String, Optional<String>> actualPdfValues = processPdf(actualPdf);
+
+                for(String label : actualPdfValues.keySet()) {
+                    assertThat(templatePdfValues.containsKey(label)).isTrue();
+                    assertThat(templatePdfValues.get(label)).isEqualTo(actualPdfValues.get(label));
+                }
+            }
+        }
+    }
+
+    private Map<String, Optional<String>> processPdf(PDDocument pdDocument) throws IOException {
+        PDDocumentCatalog pdDocumentCatalog = pdDocument.getDocumentCatalog();
+        PDAcroForm pdfForm = pdDocumentCatalog.getAcroForm();
+        Map<String, Optional<String>> returnFields = new HashMap<>();
+        pdfForm.getFields().forEach(
+            field -> {
+                Tuple fieldTuple = processField(field);
+                returnFields.put(fieldTuple.v1().toString(), Optional.ofNullable(fieldTuple.v2().toString()));
+            }
+        );
+        return returnFields;
+    }
+
+    private Tuple<String, String> processField(PDField field) {
+
+        if (field instanceof PDNonTerminalField) {
+            for (PDField child : ((PDNonTerminalField) field).getChildren()) {
+                processField(child);
+            }
+        }
+
+        return new Tuple<>(field.getFullyQualifiedName(), field.getValueAsString());
+    }
 }
