1727345a728bfaae40ce56e9bff69c905f602f17
Daryl Boam <daryl.boam@version1.com>
2022-08-17 17:52:34 +0100

    2256: add more tests and impl

--- src/main/java/uk/gov/hmcts/reform/et/syaapi/controllers/PdfMapperController.java
+++ src/main/java/uk/gov/hmcts/reform/et/syaapi/controllers/PdfMapperController.java
@@ -5,31 +5,33 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import uk.gov.hmcts.et.common.model.ccd.CaseData;
+import uk.gov.hmcts.reform.et.syaapi.helper.EmployeeObjectMapper;
+import uk.gov.hmcts.reform.et.syaapi.models.CaseRequest;
 import uk.gov.hmcts.reform.et.syaapi.service.pdf.PdfService;
 import uk.gov.hmcts.reform.et.syaapi.service.pdf.PdfServiceException;
-
-import static uk.gov.hmcts.reform.et.syaapi.constants.EtSyaConstants.AUTHORIZATION;
 
 /**
  * Rest Controller for {@link PdfService} to convert CaseData into a PDF document
  */
 @RequiredArgsConstructor
 @RestController
+@RequestMapping("/cases")
 public class PdfMapperController {
 
-    private PdfService pdfService;
+    private final PdfService pdfService;
 
-    @PostMapping(value = "case-to-pdf")
+
+    @PostMapping(value = "/convert-to-pdf")
     public ResponseEntity<byte[]> convertCaseToPdf(
-        @RequestHeader(AUTHORIZATION) String authorization,
-        @RequestBody CaseData caseData
+        @RequestBody CaseRequest caseRequest
     ) {
         byte[] pdfDocument;
         try {
-            pdfDocument = pdfService.convertCaseToPdf(caseData);
+            pdfDocument = pdfService.convertCaseToPdf(
+                new EmployeeObjectMapper().getEmploymentCaseDataFull(
+                    caseRequest.getCaseData()));
         } catch (PdfServiceException ex) {
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
--- src/test/java/uk/gov/hmcts/reform/et/syaapi/controllers/PdfMapperControllerTest.java
+++ src/test/java/uk/gov/hmcts/reform/et/syaapi/controllers/PdfMapperControllerTest.java
@@ -1,28 +1,89 @@
 package uk.gov.hmcts.reform.et.syaapi.controllers;
 
+import java.util.Map;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static uk.gov.hmcts.reform.et.syaapi.utils.TestConstants.TEST_SERVICE_AUTH_TOKEN;
+
+import lombok.SneakyThrows;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import uk.gov.hmcts.et.common.model.ccd.CaseData;
+import uk.gov.hmcts.reform.et.syaapi.models.CaseRequest;
+import uk.gov.hmcts.reform.et.syaapi.service.CaseService;
+import uk.gov.hmcts.reform.et.syaapi.service.VerifyTokenService;
 import uk.gov.hmcts.reform.et.syaapi.service.pdf.PdfService;
+import uk.gov.hmcts.reform.et.syaapi.service.pdf.PdfServiceException;
+import uk.gov.hmcts.reform.et.syaapi.utils.ResourceLoader;
 
 @WebMvcTest(
     controllers = {PdfMapperController.class}
 )
+@Import(PdfMapperController.class)
 public class PdfMapperControllerTest {
+
+    private static final String CASE_ID = "1646225213651590";
+
+    private static final Map<String, Object> CASE_DATA = Map.of();
+
     @Autowired
     private MockMvc mockMvc;
 
     @MockBean
     private PdfService pdfService;
 
+    @MockBean
+    private VerifyTokenService verifyTokenService;
+
     PdfMapperControllerTest() {
 
     }
 
+    @SneakyThrows
     @Test
-    void givenCallWithCaseProduces() {
+    void givenCallWithCaseProducesPdfDocument() {
+        CaseRequest caseRequest = CaseRequest.builder()
+            .caseId(CASE_ID).caseData(CASE_DATA).build();
 
+        when(pdfService.convertCaseToPdf(new CaseData())).thenReturn("".getBytes());
+        when(verifyTokenService.verifyTokenSignature(any())).thenReturn(true);
+
+        mockMvc.perform(post("/cases/convert-to-pdf")
+                .header(HttpHeaders.AUTHORIZATION, TEST_SERVICE_AUTH_TOKEN)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(ResourceLoader.toJson(caseRequest)))
+            .andExpect(status().isOk());
     }
+
+    @SneakyThrows
+    @Test
+    void givenPdfServiceExpectionProducesServerError() {
+        CaseRequest caseRequest = CaseRequest.builder()
+            .caseId(CASE_ID).caseData(CASE_DATA).build();
+
+        when(pdfService.convertCaseToPdf(new CaseData())).thenThrow(PdfServiceException.class);
+        when(verifyTokenService.verifyTokenSignature(any())).thenReturn(true);
+
+        mockMvc.perform(post("/cases/convert-to-pdf")
+                .header(HttpHeaders.AUTHORIZATION, TEST_SERVICE_AUTH_TOKEN)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(ResourceLoader.toJson(caseRequest)))
+            .andExpect(status().isInternalServerError());
+    }
+
+//
+//    @SneakyThrows
+//    @Test
+//    void givenEmptyCaseProduces() {
+//
+//    }
 }
