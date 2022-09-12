0fcbc3084d1d9b2d9aa197308a24d61d9a60e4c7
hsjhita <harpreet.jhita@justice.gov.uk>
2022-09-12 12:00:37 +0100

    RET-2295
    RET-2295
    
    Java Doc and checkstyle
    
    Checkstyle
    
    PMD
    
    PMD
    
    Sonar
    
    Imports

--- src/main/java/uk/gov/hmcts/reform/et/syaapi/config/AuthConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/et/syaapi/config/AuthConfiguration.java
@@ -1,6 +1,5 @@
 package uk.gov.hmcts.reform.et.syaapi.config;
 
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
