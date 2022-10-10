75bd548596f329b85df6c5284adfb96ae4745c2f
robin-ieong <101253854+robin-ieong@users.noreply.github.com>
2022-10-10 11:43:39 +0100

    Removed logs what are not requried for prod (#161)
    * Removed logs what are not requried for prod
    
    * Refactored for checkstyle
    
    * removed blank lines

--- src/main/java/uk/gov/hmcts/reform/et/syaapi/service/CaseDocumentService.java
+++ src/main/java/uk/gov/hmcts/reform/et/syaapi/service/CaseDocumentService.java
@@ -145,12 +145,10 @@ public class CaseDocumentService {
      * @throws ResourceNotFoundException if the target API returns 404 response code
      */
     public ResponseEntity<CaseDocument> getDocumentDetails(String authToken, UUID documentId) {
-        log.info("Called getDocumentDetails in CaseDocumentService, doc id: " + documentId.toString());
         HttpHeaders headers = new HttpHeaders();
         headers.add(HttpHeaders.AUTHORIZATION, authToken);
         headers.add(SERVICE_AUTHORIZATION, authTokenGenerator.generate());
         HttpEntity<MultiValueMap<String, Object>> request = new HttpEntity<>(headers);
-        log.info("getDocumentDetails - Added authorization");
         try {
             ResponseEntity<CaseDocument> response = restTemplate.exchange(
                 caseDocApiUrl + "/cases/documents/" + documentId,
@@ -158,28 +156,26 @@ public class CaseDocumentService {
                 request,
                 CaseDocument.class
             );
-            log.info("getDocumentDetails - got the response: " + response.toString());
-            HttpHeaders responseHeaders = new HttpHeaders();
-            responseHeaders.add("Connection", "keep-alive");
-            responseHeaders.add("Content-Type", "application/json");
-            responseHeaders.add("X-Frame-Options", "DENY");
-            responseHeaders.add("X-XSS-Protection", "1; mode=block");
-            responseHeaders.add("X-Content-Type-Options", "nosniff");
-            return new ResponseEntity<>(response.getBody(), responseHeaders, HttpStatus.OK);
+
+            return new ResponseEntity<>(response.getBody(), getResponseHeaders(), HttpStatus.OK);
         } catch (HttpClientErrorException ex) {
-            log.error("getDocumentDetails exception code: " + ex.getStatusCode());
-            log.error("getDocumentDetails exception message: " + ex.getMessage());
-            log.error("getDocumentDetails exception response string: " + ex.getResponseBodyAsString());
             if (NOT_FOUND.equals(ex.getStatusCode())) {
                 throw new ResourceNotFoundException(String.format(RESOURCE_NOT_FOUND,
                                                                   documentId, ex.getMessage()), ex);
             }
             throw ex;
-        } catch (Exception ex) {
-            log.error("getDocumentDetails general exception message: " + ex.getMessage());
-            log.error("getDocumentDetails general exception cause: " + ex.getCause());
-            throw ex;
         }
+    }
+
+    private HttpHeaders getResponseHeaders() {
+        HttpHeaders responseHeaders = new HttpHeaders();
+        responseHeaders.add("Connection", "keep-alive");
+        responseHeaders.add("Content-Type", "application/json");
+        responseHeaders.add("X-Frame-Options", "DENY");
+        responseHeaders.add("X-XSS-Protection", "1; mode=block");
+        responseHeaders.add("X-Content-Type-Options", "nosniff");
+
+        return responseHeaders;
     }
 
     private DocumentUploadResponse attemptWithRetriesToUploadDocumentToCaseDocumentApi(int attempts,
