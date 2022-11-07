ec4d924557d4a5651108124671e75fe5069722e1
Itay-Kirsh <Itayk747@gmail.com>
2022-11-07 14:49:56 +0000

    RET-2777. Split methods further to solve PMD issues.

--- src/main/java/uk/gov/hmcts/reform/et/syaapi/service/pdf/PersonalDetailsMapper.java
+++ src/main/java/uk/gov/hmcts/reform/et/syaapi/service/pdf/PersonalDetailsMapper.java
@@ -36,99 +36,116 @@ public class PersonalDetailsMapper {
     );
 
     private static final String CLAIMANT_ADDRESS_PREFIX = "1.5";
+    private static final String PHONE_NUMBER_PREFIX = "1.6";
 
-    // todo split further and add tests.
+    // todo add tests.
     public Map<String, Optional<String>> printPersonalDetails(CaseData caseData) {
-        ConcurrentHashMap<String, Optional<String>> printFields = new ConcurrentHashMap<>();
+        Map<String, Optional<String>> printFields = new ConcurrentHashMap<>();
 
-        ClaimantIndType claimantIndType = caseData.getClaimantIndType();
-        if (claimantIndType != null) {
-            if (claimantIndType.getClaimantPreferredTitle() != null
-                && TITLES.containsKey(claimantIndType.getClaimantPreferredTitle())) {
+        printFields.putAll(mapClaimantIndType(caseData.getClaimantIndType()));
+        printFields.putAll(mapClaimantType(caseData.getClaimantType()));
+
+        return printFields;
+    }
+
+    private Map<String, Optional<String>> mapClaimantIndType(ClaimantIndType claimantIndType) {
+        Map<String, Optional<String>> printFields = new ConcurrentHashMap<>();
+        if (claimantIndType == null) {
+            return printFields;
+        }
+
+        if (claimantIndType.getClaimantPreferredTitle() != null
+            && TITLES.containsKey(claimantIndType.getClaimantPreferredTitle())) {
+            printFields.put(
+                TITLES.get(claimantIndType.getClaimantPreferredTitle()),
+                ofNullable(TITLE_MAP.get(claimantIndType.getClaimantPreferredTitle()))
+            );
+            if (OTHER.equals(claimantIndType.getClaimantPreferredTitle())) {
                 printFields.put(
-                    TITLES.get(claimantIndType.getClaimantPreferredTitle()),
-                    ofNullable(TITLE_MAP.get(claimantIndType.getClaimantPreferredTitle()))
+                    TITLES.get("Other_Specify"),
+                    ofNullable(String.valueOf(claimantIndType.getClaimantTitleOther()))
                 );
-                if (OTHER.equals(claimantIndType.getClaimantPreferredTitle())) {
-                    printFields.put(
-                        TITLES.get("Other_Specify"),
-                        ofNullable(String.valueOf(claimantIndType.getClaimantTitleOther()))
-                    );
-                }
-            }
-
-            printFields.put(
-                PdfMapperConstants.Q1_FIRST_NAME,
-                ofNullable(claimantIndType.getClaimantFirstNames())
-            );
-
-            printFields.put(
-                PdfMapperConstants.Q1_SURNAME,
-                ofNullable(claimantIndType.getClaimantLastName())
-            );
-
-            printFields.putAll(getDobPrintFields(claimantIndType.getClaimantDateOfBirth()));
-
-            if (claimantIndType.getClaimantSex() != null) {
-                if ("Male".equals(claimantIndType.getClaimantSex())) {
-                    printFields.put(PdfMapperConstants.Q1_SEX_MALE, Optional.of("Yes"));
-                } else if ("Female".equals(claimantIndType.getClaimantSex())) {
-                    printFields.put(PdfMapperConstants.Q1_SEX_FEMALE, Optional.of("female"));
-                } else if ("Prefer not to say".equals(claimantIndType.getClaimantSex())) {
-                    printFields.put(PdfMapperConstants.Q1_SEX_PREFER_NOT_TO_SAY, Optional.of("prefer not to say"));
-                }
             }
         }
 
-        ClaimantType claimantType = caseData.getClaimantType();
-        if (claimantType != null) {
-            Address claimantAddressUK = claimantType.getClaimantAddressUK();
-            if (claimantAddressUK != null) {
-                printFields.put(
-                    String.format(PdfMapperConstants.RP2_HOUSE_NUMBER, CLAIMANT_ADDRESS_PREFIX),
-                    ofNullable(claimantAddressUK.getAddressLine1())
-                );
-                printFields.put(
-                    String.format(PdfMapperConstants.QX_STREET, CLAIMANT_ADDRESS_PREFIX),
-                    ofNullable(claimantAddressUK.getAddressLine2())
-                );
-                printFields.put(
-                    String.format(PdfMapperConstants.RP_POST_TOWN, CLAIMANT_ADDRESS_PREFIX),
-                    ofNullable(claimantAddressUK.getPostTown())
-                );
-                printFields.put(
-                    String.format(PdfMapperConstants.QX_COUNTY, CLAIMANT_ADDRESS_PREFIX),
-                    ofNullable(claimantAddressUK.getCounty())
-                );
-                printFields.put(
-                    String.format(PdfMapperConstants.QX_POSTCODE, CLAIMANT_ADDRESS_PREFIX),
-                    ofNullable(formatPostcode(claimantAddressUK.getPostCode()))
-                );
-            }
+        printFields.put(
+            PdfMapperConstants.Q1_FIRST_NAME,
+            ofNullable(claimantIndType.getClaimantFirstNames())
+        );
 
-            printFields.put(
-                String.format(PdfMapperConstants.QX_PHONE_NUMBER, "1.6"),
-                ofNullable(claimantType.getClaimantPhoneNumber())
-            );
+        printFields.put(
+            PdfMapperConstants.Q1_SURNAME,
+            ofNullable(claimantIndType.getClaimantLastName())
+        );
 
-            printFields.put(
-                PdfMapperConstants.Q1_MOBILE_NUMBER,
-                ofNullable(claimantType.getClaimantMobileNumber())
-            );
+        printFields.putAll(getDobPrintFields(claimantIndType.getClaimantDateOfBirth()));
 
-            printFields.put(
-                PdfMapperConstants.Q1_EMAIL,
-                ofNullable(claimantType.getClaimantEmailAddress())
-            );
-
-            String contactPreference = claimantType.getClaimantContactPreference();
-
-            if (EMAIL.equals(contactPreference)) {
-                printFields.put(PdfMapperConstants.Q1_CONTACT_EMAIL, Optional.of(contactPreference));
-            } else if ("Post".equals(contactPreference)) {
-                printFields.put(PdfMapperConstants.Q1_CONTACT_POST, Optional.of(contactPreference));
+        if (claimantIndType.getClaimantSex() != null) {
+            if ("Male".equals(claimantIndType.getClaimantSex())) {
+                printFields.put(PdfMapperConstants.Q1_SEX_MALE, Optional.of("Yes"));
+            } else if ("Female".equals(claimantIndType.getClaimantSex())) {
+                printFields.put(PdfMapperConstants.Q1_SEX_FEMALE, Optional.of("female"));
+            } else if ("Prefer not to say".equals(claimantIndType.getClaimantSex())) {
+                printFields.put(PdfMapperConstants.Q1_SEX_PREFER_NOT_TO_SAY, Optional.of("prefer not to say"));
             }
         }
+
+        return printFields;
+    }
+
+    private Map<String, Optional<String>> mapClaimantType(ClaimantType claimantType) {
+        Map<String, Optional<String>> printFields = new ConcurrentHashMap<>();
+        if (claimantType == null) {
+            return printFields;
+        }
+
+        Address claimantAddressUK = claimantType.getClaimantAddressUK();
+        if (claimantAddressUK != null) {
+            printFields.put(
+                String.format(PdfMapperConstants.RP2_HOUSE_NUMBER, CLAIMANT_ADDRESS_PREFIX),
+                ofNullable(claimantAddressUK.getAddressLine1())
+            );
+            printFields.put(
+                String.format(PdfMapperConstants.QX_STREET, CLAIMANT_ADDRESS_PREFIX),
+                ofNullable(claimantAddressUK.getAddressLine2())
+            );
+            printFields.put(
+                String.format(PdfMapperConstants.RP_POST_TOWN, CLAIMANT_ADDRESS_PREFIX),
+                ofNullable(claimantAddressUK.getPostTown())
+            );
+            printFields.put(
+                String.format(PdfMapperConstants.QX_COUNTY, CLAIMANT_ADDRESS_PREFIX),
+                ofNullable(claimantAddressUK.getCounty())
+            );
+            printFields.put(
+                String.format(PdfMapperConstants.QX_POSTCODE, CLAIMANT_ADDRESS_PREFIX),
+                ofNullable(formatPostcode(claimantAddressUK.getPostCode()))
+            );
+        }
+
+        printFields.put(
+            String.format(PdfMapperConstants.QX_PHONE_NUMBER, PHONE_NUMBER_PREFIX),
+            ofNullable(claimantType.getClaimantPhoneNumber())
+        );
+
+        printFields.put(
+            PdfMapperConstants.Q1_MOBILE_NUMBER,
+            ofNullable(claimantType.getClaimantMobileNumber())
+        );
+
+        printFields.put(
+            PdfMapperConstants.Q1_EMAIL,
+            ofNullable(claimantType.getClaimantEmailAddress())
+        );
+
+        String contactPreference = claimantType.getClaimantContactPreference();
+
+        if (EMAIL.equals(contactPreference)) {
+            printFields.put(PdfMapperConstants.Q1_CONTACT_EMAIL, Optional.of(contactPreference));
+        } else if ("Post".equals(contactPreference)) {
+            printFields.put(PdfMapperConstants.Q1_CONTACT_POST, Optional.of(contactPreference));
+        }
+
         return printFields;
     }
 
