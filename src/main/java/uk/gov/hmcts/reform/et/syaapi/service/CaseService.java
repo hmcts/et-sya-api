dc79923240a276b8effa9bcb76b2caab349bb6bf
gauravchawla <gaurav-chawla@hotmail.com>
2022-11-17 10:56:09 +0000

    RET-2792

--- src/main/java/uk/gov/hmcts/reform/et/syaapi/service/CaseService.java
+++ src/main/java/uk/gov/hmcts/reform/et/syaapi/service/CaseService.java
@@ -216,10 +216,10 @@ public class CaseService {
                                                           casePdfFile,
                                                           acasCertificates));
         String emailTemplateId = notificationsProperties.getSubmitCaseEmailTemplateId();
-        String citizenPortalLink = notificationsProperties.getCitizenPortalLink();
+        String citizenPortalLink = notificationsProperties.getCitizenPortalLink() + "{0}";
         if (WELSH_LANGUAGE.equals(caseData.getClaimantType().getClaimantContactLanguage())) {
             emailTemplateId = notificationsProperties.getCySubmitCaseEmailTemplateId();
-            citizenPortalLink = notificationsProperties.getCitizenPortalLink() + "?lng=cy/";
+            citizenPortalLink = notificationsProperties.getCitizenPortalLink() + "{0} + /?lng=cy";
         }
         triggerEvent(authorization, caseRequest.getCaseId(), UPDATE_CASE_SUBMITTED, caseDetails.getCaseTypeId(),
                      caseDetails.getData());
