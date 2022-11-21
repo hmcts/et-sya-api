6b6a6900bf61756bf77a21322de83ce4b3a31951
KirsanovI <ivan.kirsanov@hmcts.net>
2022-11-21 15:51:45 +0100

    RET-2750: Chekstyle fix

--- src/functionalTest/java/uk/gov/hmcts/reform/et/syaapi/DocumentUploadControllerFunctionalTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/et/syaapi/DocumentUploadControllerFunctionalTest.java
@@ -1,7 +1,6 @@
 package uk.gov.hmcts.reform.et.syaapi;
 
 import io.restassured.RestAssured;
-import io.restassured.http.ContentType;
 import io.restassured.http.Header;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.http.HttpStatus;
