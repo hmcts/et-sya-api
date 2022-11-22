e18a012e28abca99f339ebe31df5a9148db4d948
KirsanovI <ivan.kirsanov@hmcts.net>
2022-11-22 15:24:59 +0100

    RET-2750: Add functional tests for DocumentController

---
+++ src/functionalTest/java/uk/gov/hmcts/reform/et/syaapi/DocumentControllerFunctionalTest.java
@@ -0,0 +1,66 @@
+package uk.gov.hmcts.reform.et.syaapi;
+
+import io.restassured.RestAssured;
+import io.restassured.http.Header;
+import org.apache.http.HttpStatus;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.MediaType;
+
+import java.io.File;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+class DocumentControllerFunctionalTest extends BaseFunctionalTest {
+    private static final String CASE_TYPE = "ET_EnglandWales";
+    private static final String AUTHORIZATION = "Authorization";
+    private static final File UPLOAD_TEST_FILE = new File("src/functionalTest/resources/uploads/zipcode.txt");
+
+    @Test
+    void shoudGetUploadedDocumentBinaryContent() {
+        String retrievedDoc = RestAssured.given()
+            .header(new Header(AUTHORIZATION, userToken))
+            .contentType(MediaType.APPLICATION_JSON_VALUE)
+            .when()
+            .get("/document/download/" + retrieveUploadedFileId())
+            .then()
+            .statusCode(HttpStatus.SC_OK)
+            .log().all(true)
+            .extract().asString();
+
+        assertThat(retrievedDoc).isNotNull();
+        assertThat(retrievedDoc).contains("LS11AA");
+    }
+
+    @Test
+    void shoudGetUploadedDocumentDetails() {
+        RestAssured.given()
+            .header(new Header(AUTHORIZATION, userToken))
+            .contentType(MediaType.APPLICATION_JSON_VALUE)
+            .when()
+            .get("/document/download/" + retrieveUploadedFileId())
+            .then()
+            .statusCode(HttpStatus.SC_OK)
+            .log().all(true)
+            .assertThat().body("mimeType", equalTo("text/plain"))
+            .assertThat().body("metadata.originalDocumentName", equalTo(UPLOAD_TEST_FILE.getName()));
+    }
+
+    private String retrieveUploadedFileId() {
+        String uploadedDocUri = RestAssured.given()
+            .header(new Header(AUTHORIZATION, userToken))
+            .contentType(MediaType.MULTIPART_FORM_DATA_VALUE)
+            .multiPart("document_upload", UPLOAD_TEST_FILE, MediaType.APPLICATION_JSON_VALUE)
+            .when()
+            .post("/documents/upload/" + CASE_TYPE)
+            .then()
+            .statusCode(HttpStatus.SC_OK)
+            .extract()
+            .body()
+            .jsonPath()
+            .get("uri");
+
+        uploadedDocUri = uploadedDocUri.replaceAll(".*documents\\/", "");
+        return uploadedDocUri;
+    }
+}
