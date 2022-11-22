ae2d0549d5f6bb2e85c51454edc9edec4cee38a9
KirsanovI <ivan.kirsanov@hmcts.net>
2022-11-22 08:35:36 +0100

    RET-2750: Add functional tests to DocumentUploadController

--- src/functionalTest/java/uk/gov/hmcts/reform/et/syaapi/DocumentUploadControllerFunctionalTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/et/syaapi/DocumentUploadControllerFunctionalTest.java
@@ -2,37 +2,25 @@ package uk.gov.hmcts.reform.et.syaapi;
 
 import io.restassured.RestAssured;
 import io.restassured.http.Header;
-import lombok.extern.slf4j.Slf4j;
 import org.apache.http.HttpStatus;
-import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.http.MediaType;
-import org.springframework.mock.web.MockMultipartFile;
+
+import java.io.File;
 
 import static org.hamcrest.Matchers.equalTo;
 
-@Slf4j
-@TestMethodOrder(MethodOrderer.MethodName.class)
 class DocumentUploadControllerFunctionalTest extends BaseFunctionalTest {
     private static final String CASE_TYPE = "ET_EnglandWales";
     private static final String AUTHORIZATION = "Authorization";
-    private static final String DOCUMENT_NAME = "hello.txt";
-    private static final String MOCK_FILE_BODY = "Hello, World!";
-    private static final MockMultipartFile MOCK_FILE = new MockMultipartFile(
-        "document_upload",
-        DOCUMENT_NAME,
-        MediaType.TEXT_PLAIN_VALUE,
-        MOCK_FILE_BODY.getBytes()
-    );
+    private static final File UPLOAD_TEST_FILE = new File("src/functionalTest/resources/uploads/zipcode.txt");
 
     @Test
     void uploadDocumentShouldReturnCaseDocument() {
         RestAssured.given()
-            .contentType(MediaType.MULTIPART_FORM_DATA_VALUE)
             .header(new Header(AUTHORIZATION, userToken))
-            .multiPart("document_upload", MOCK_FILE)
-            .when()
+            .multiPart("document_upload", UPLOAD_TEST_FILE, MediaType.APPLICATION_JSON_VALUE)
+            .contentType(MediaType.MULTIPART_FORM_DATA_VALUE)
             .post("/documents/upload/" + CASE_TYPE)
             .then()
             .statusCode(HttpStatus.SC_OK)
@@ -44,9 +32,8 @@ class DocumentUploadControllerFunctionalTest extends BaseFunctionalTest {
     @Test
     void uploadDocumentShouldReturnBadRequestIfCaseTypeIdDoesntMatch() {
         RestAssured.given()
-            .contentType(MediaType.MULTIPART_FORM_DATA_VALUE)
             .header(new Header(AUTHORIZATION, userToken))
-            .multiPart("document_upload", MOCK_FILE)
+            .multiPart("document_upload", UPLOAD_TEST_FILE, MediaType.APPLICATION_JSON_VALUE)
             .post("/documents/upload/WrongCaseTypeId")
             .then()
             .statusCode(HttpStatus.SC_BAD_REQUEST)
