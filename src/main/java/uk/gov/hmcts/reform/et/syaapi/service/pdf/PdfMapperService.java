1e24de80bfc9f2c7513d968b2e7beafb5ca323cd
Mehmet Tahir Dede <105645217+JustMehmet@users.noreply.github.com>
2023-01-26 15:30:28 +0000

    Ret-3086 Change date formats on ET1 PDF output from YYYY/MM/DD to DD/MM/YYYY (#248)
    * Update for formatting date fields as dd-MM-yyyy.
    
    * Update for formatting date fields as dd-MM-yyyy.
    
    * Update for formatting date fields as dd-MM-yyyy.

--- src/main/java/uk/gov/hmcts/reform/et/syaapi/service/pdf/PdfMapperService.java
+++ src/main/java/uk/gov/hmcts/reform/et/syaapi/service/pdf/PdfMapperService.java
@@ -116,7 +116,8 @@ public class PdfMapperService {
         try {
             printFields.put(PdfMapperConstants.TRIBUNAL_OFFICE, ofNullable(caseData.getManagingOffice()));
             printFields.put(PdfMapperConstants.CASE_NUMBER, ofNullable(caseData.getEthosCaseReference()));
-            printFields.put(PdfMapperConstants.DATE_RECEIVED, ofNullable(caseData.getReceiptDate()));
+            printFields.put(PdfMapperConstants.DATE_RECEIVED,
+                            ofNullable(PdfMapperUtil.formatDate(caseData.getReceiptDate())));
             printFields.putAll(printHearingPreferences(caseData));
             printFields.putAll(printRespondentDetails(caseData));
             printFields.putAll(printMultipleClaimsDetails(caseData));
@@ -309,7 +310,7 @@ public class PdfMapperService {
 
                 printFields.put(
                     PdfMapperConstants.Q5_EMPLOYMENT_START,
-                    ofNullable(claimantOtherType.getClaimantEmployedFrom())
+                    ofNullable(PdfMapperUtil.formatDate(claimantOtherType.getClaimantEmployedFrom()))
                 );
 
                 String stillWorking = NO_LONGER_WORKING.equals(claimantOtherType.getStillWorking()) ? NO :
@@ -319,7 +320,7 @@ public class PdfMapperService {
                     if (NOTICE.equals(claimantOtherType.getStillWorking())) {
                         printFields.put(
                             PdfMapperConstants.Q5_NOT_ENDED,
-                            ofNullable(claimantOtherType.getClaimantEmployedNoticePeriod())
+                            ofNullable(PdfMapperUtil.formatDate(claimantOtherType.getClaimantEmployedNoticePeriod()))
                         );
                     }
 
@@ -327,7 +328,7 @@ public class PdfMapperService {
                     printFields.put(PdfMapperConstants.Q5_CONTINUING_NO, Optional.of(NO_LOWERCASE));
                     printFields.put(
                         PdfMapperConstants.Q5_EMPLOYMENT_END,
-                        ofNullable(claimantOtherType.getClaimantEmployedTo())
+                        ofNullable(PdfMapperUtil.formatDate(claimantOtherType.getClaimantEmployedTo()))
                     );
                 }
 
@@ -356,7 +357,7 @@ public class PdfMapperService {
                 printFields.put(PdfMapperConstants.Q7_OTHER_JOB_YES, Optional.of(YES));
                 printFields.put(
                     PdfMapperConstants.Q7_START_WORK,
-                    ofNullable(newEmploymentType.getNewlyEmployedFrom())
+                    ofNullable(PdfMapperUtil.formatDate(newEmploymentType.getNewlyEmployedFrom()))
                 );
                 printFields.put(
                     PdfMapperConstants.Q7_EARNING,
