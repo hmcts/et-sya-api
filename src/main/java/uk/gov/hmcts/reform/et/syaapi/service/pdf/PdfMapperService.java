1f68027ad496afbc613b22644f48d01f9caa335c
Mehmet Tahir Dede <105645217+JustMehmet@users.noreply.github.com>
2023-02-15 11:59:53 +0000

    Pet pi28 s6 (#266)
    * RET-2903 and RET-2961
    
    * Hot fix - Ret 2899 new (#230)
    
    * log
    
    * CVE
    
    * Added a new control on line 450 to check pension contribution value as NO to select Pension No option. (#236)
    
    * Check for CVE
    
    * Revert "Check for CVE"
    
    This reverts commit def7dc166fc958162b4a9832a0be1d49067de776.
    
    * Check for CVE
    
    Revert "Check for CVE"
    
    This reverts commit def7dc166fc958162b4a9832a0be1d49067de776.
    
    * Ret-3043 Updates for PDF Creation bugs (#241)
    
    * Updates for PDF creation bugs.
    
    * Updates for PDF creation bugs.
    
    * Package update for StringUtils (#244)
    
    * PET-1.1.0.7 (#255)
    
    * Upgrade to helm version 4.0.11 (#254)
    
    * Ret 3134 Terraform & Azurerm Upgrade (#256)
    
    * Upgrades for terraform from 0.13.5 to 1.3.7, and azurerm from 2.25 to 3.33.0
    
    * Upgrades for terraform from 0.13.5 to 1.3.7, and azurerm from 2.25 to 3.33.0
    
    * Upgrades for terraform from 0.13.5 to 1.3.7, and azurerm from 2.25 to 3.33.0
    
    * Upgrades for terraform from 0.13.5 to 1.3.7, and azurerm from 2.25 to 3.33.0
    
    * RET-2997 (#258)
    
    ---------
    
    Co-authored-by: GauravChawlaMOJ <58194626+GauravChawlaMOJ@users.noreply.github.com>
    
    * TerraformForAPIAccount (#263)
    
    * RET-2296 Chart keys (#265)
    
    * StringUtils.isNotEmpty method is replaced with !Strings.isNullOrEmpty on line 261
    
    ---------
    
    Co-authored-by: gauravchawla <gaurav-chawla@hotmail.com>
    Co-authored-by: GauravChawlaMOJ <58194626+GauravChawlaMOJ@users.noreply.github.com>
    Co-authored-by: Harpreet Jhita <harpreet.jhita@justice.gov.uk>

--- src/main/java/uk/gov/hmcts/reform/et/syaapi/service/pdf/PdfMapperService.java
+++ src/main/java/uk/gov/hmcts/reform/et/syaapi/service/pdf/PdfMapperService.java
@@ -258,21 +258,23 @@ public class PdfMapperService {
                 printFields.put(String.format(
                     PdfMapperConstants.QX_HAVE_ACAS_NO, questionPrefix), Optional.of(NO_LOWERCASE));
             }
-            switch (respondent.getRespondentAcasNo()) {
-                case "Unfair Dismissal":
-                    printFields.put(String.format(PdfMapperConstants.QX_ACAS_A1, questionPrefix), Optional.of(YES));
-                    break;
-                case "Another person":
-                    printFields.put(String.format(PdfMapperConstants.QX_ACAS_A2, questionPrefix), Optional.of(YES));
-                    break;
-                case "No Power":
-                    printFields.put(String.format(PdfMapperConstants.QX_ACAS_A3, questionPrefix), Optional.of(YES));
-                    break;
-                case "Employer already in touch":
-                    printFields.put(String.format(PdfMapperConstants.QX_ACAS_A4, questionPrefix), Optional.of(YES));
-                    break;
-                default:
-                    break;
+            if (!Strings.isNullOrEmpty(respondent.getRespondentAcasNo())) {
+                switch (respondent.getRespondentAcasNo()) {
+                    case "Unfair Dismissal":
+                        printFields.put(String.format(PdfMapperConstants.QX_ACAS_A1, questionPrefix), Optional.of(YES));
+                        break;
+                    case "Another person":
+                        printFields.put(String.format(PdfMapperConstants.QX_ACAS_A2, questionPrefix), Optional.of(YES));
+                        break;
+                    case "No Power":
+                        printFields.put(String.format(PdfMapperConstants.QX_ACAS_A3, questionPrefix), Optional.of(YES));
+                        break;
+                    case "Employer already in touch":
+                        printFields.put(String.format(PdfMapperConstants.QX_ACAS_A4, questionPrefix), Optional.of(YES));
+                        break;
+                    default:
+                        break;
+                }
             }
         }
         return printFields;
