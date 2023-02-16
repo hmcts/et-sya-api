e6ebb8548822773ca1172ee1d01bedc2fc52e398
ikirsanov <ivan.kirsanov@version1.com>
2023-02-16 08:47:04 +0100

    RET-2758: Fix checkstyle

--- src/main/java/uk/gov/hmcts/reform/et/syaapi/controllers/ManageCaseController.java
+++ src/main/java/uk/gov/hmcts/reform/et/syaapi/controllers/ManageCaseController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
-import uk.gov.hmcts.et.common.model.ccd.types.citizenhub.ClaimantTse;
 import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
 import uk.gov.hmcts.reform.et.syaapi.annotation.ApiResponseGroup;
 import uk.gov.hmcts.reform.et.syaapi.enums.CaseEvent;
@@ -23,7 +22,6 @@ import uk.gov.hmcts.reform.et.syaapi.models.HubLinksStatusesRequest;
 import uk.gov.hmcts.reform.et.syaapi.service.ApplicationService;
 import uk.gov.hmcts.reform.et.syaapi.service.CaseDocumentException;
 import uk.gov.hmcts.reform.et.syaapi.service.CaseService;
-import uk.gov.hmcts.reform.et.syaapi.service.DocumentGenerationException;
 import uk.gov.hmcts.reform.et.syaapi.service.pdf.PdfServiceException;
 
 import java.util.List;
