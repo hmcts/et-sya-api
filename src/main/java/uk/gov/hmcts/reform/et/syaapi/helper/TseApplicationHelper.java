d428a1224609be278e2e9ab33f834131c924bf16
marcjudge <98343606+marcjudge@users.noreply.github.com>
2023-03-16 14:41:57 +0000

    Fix saving doc upload (#300)

--- src/main/java/uk/gov/hmcts/reform/et/syaapi/helper/TseApplicationHelper.java
+++ src/main/java/uk/gov/hmcts/reform/et/syaapi/helper/TseApplicationHelper.java
@@ -4,17 +4,22 @@ import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.collections4.CollectionUtils;
+import uk.gov.hmcts.et.common.model.ccd.CaseData;
+import uk.gov.hmcts.et.common.model.ccd.items.DocumentTypeItem;
 import uk.gov.hmcts.et.common.model.ccd.items.GenericTseApplicationType;
 import uk.gov.hmcts.et.common.model.ccd.items.GenericTseApplicationTypeItem;
 import uk.gov.hmcts.et.common.model.ccd.items.TseRespondTypeItem;
 import uk.gov.hmcts.et.common.model.ccd.types.TseRespondType;
 import uk.gov.hmcts.reform.et.syaapi.models.RespondToApplicationRequest;
+import uk.gov.hmcts.reform.et.syaapi.service.CaseDocumentService;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
+
+import static uk.gov.hmcts.reform.et.syaapi.constants.EtSyaConstants.CLAIMANT_CORRESPONDENCE_DOCUMENT;
 
 @Slf4j
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
@@ -39,13 +44,27 @@ public final class TseApplicationHelper {
     }
 
     public static void setRespondentApplicationWithResponse(RespondToApplicationRequest request,
-                                                            GenericTseApplicationType appToModify) {
+                                                            GenericTseApplicationType appToModify,
+                                                            CaseData caseData,
+                                                            CaseDocumentService caseDocumentService) {
         if (CollectionUtils.isEmpty(appToModify.getRespondCollection())) {
             appToModify.setRespondCollection(new ArrayList<>());
         }
         TseRespondType responseToAdd = request.getResponse();
         responseToAdd.setDate(TseApplicationHelper.formatCurrentDate(LocalDate.now()));
         responseToAdd.setFrom(CLAIMANT);
+
+        if (request.getSupportingMaterialFile() != null) {
+            DocumentTypeItem documentTypeItem = caseDocumentService.createDocumentTypeItem(
+                CLAIMANT_CORRESPONDENCE_DOCUMENT,
+                request.getSupportingMaterialFile()
+            );
+
+            caseData.getDocumentCollection().add(documentTypeItem);
+
+            responseToAdd.setSupportingMaterial(new ArrayList<>());
+            responseToAdd.getSupportingMaterial().add(documentTypeItem);
+        }
 
         appToModify.getRespondCollection().add(TseRespondTypeItem.builder()
                                                    .id(UUID.randomUUID().toString())
