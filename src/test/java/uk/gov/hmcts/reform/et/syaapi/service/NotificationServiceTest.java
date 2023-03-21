3f5d2c24407b0300f6b0d9ffec2eb6b7f851fcd5
btensay <tensay.bulcha@gmail.com>
2023-03-21 09:47:56 +0000

    unused import removed

--- src/test/java/uk/gov/hmcts/reform/et/syaapi/service/NotificationServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/et/syaapi/service/NotificationServiceTest.java
@@ -28,7 +28,6 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.ArgumentMatchers.nullable;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.doReturn;
