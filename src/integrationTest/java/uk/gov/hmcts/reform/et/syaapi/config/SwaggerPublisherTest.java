9f3f2985f45c5717b671094bde427f02e080fc36
gauravchawla <gaurav-chawla@hotmail.com>
2023-04-18 00:18:54 +0100

    RET-3098

--- src/integrationTest/java/uk/gov/hmcts/reform/et/syaapi/config/SwaggerPublisherTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/et/syaapi/config/SwaggerPublisherTest.java
@@ -7,9 +7,11 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.context.WebApplicationContext;
+
 import java.io.OutputStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
