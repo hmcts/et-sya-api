b825d75e1c1857e243c2e1ac07962a56cc270aad
gauravchawla <gaurav-chawla@hotmail.com>
2023-04-18 00:21:23 +0100

    RET-3098

--- src/integrationTest/java/uk/gov/hmcts/reform/et/syaapi/config/SwaggerPublisherTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/et/syaapi/config/SwaggerPublisherTest.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.web.context.WebApplicationContext;
 
 import java.io.OutputStream;
 import java.nio.file.Files;
--- src/integrationTest/java/uk/gov/hmcts/reform/et/syaapi/controllers/GetWelcomeTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/et/syaapi/controllers/GetWelcomeTest.java
@@ -7,7 +7,6 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.web.context.WebApplicationContext;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
