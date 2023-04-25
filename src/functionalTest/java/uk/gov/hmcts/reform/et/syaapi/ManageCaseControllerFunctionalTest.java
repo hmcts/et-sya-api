44c17790afeb170acc2ca4e6bec76b9aa7c223fc
Harpreet Jhita <harpreet.jhita@justice.gov.uk>
2023-04-25 15:22:07 +0100

    Checkstyle

--- src/functionalTest/java/uk/gov/hmcts/reform/et/syaapi/ManageCaseControllerFunctionalTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/et/syaapi/ManageCaseControllerFunctionalTest.java
@@ -16,7 +16,6 @@ import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.TimeUnit;
 
-import static java.util.concurrent.TimeUnit.SECONDS;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertEquals;
