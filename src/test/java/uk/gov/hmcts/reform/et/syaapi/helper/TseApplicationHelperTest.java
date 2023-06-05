8aad333fc51a4b58e9a8d7626755378724a469ab
Itay-Kirsh <Itayk747@gmail.com>
2023-06-05 08:00:14 +0100

    Update test to include more appropriate data

--- src/test/java/uk/gov/hmcts/reform/et/syaapi/helper/TseApplicationHelperTest.java
+++ src/test/java/uk/gov/hmcts/reform/et/syaapi/helper/TseApplicationHelperTest.java
@@ -1,29 +1,32 @@
 package uk.gov.hmcts.reform.et.syaapi.helper;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import uk.gov.hmcts.et.common.model.ccd.CaseData;
 import uk.gov.hmcts.et.common.model.ccd.items.GenericTseApplicationType;
-import uk.gov.hmcts.et.common.model.ccd.types.TseRespondType;
+import uk.gov.hmcts.reform.et.syaapi.model.TestData;
 import uk.gov.hmcts.reform.et.syaapi.models.RespondToApplicationRequest;
 import uk.gov.hmcts.reform.et.syaapi.service.CaseDocumentService;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mock;
 
 class TseApplicationHelperTest {
 
-    @Test
-    void givenRespApp_whenSettingClaimantResponse_thenStatusIsWaitingForTheTribunal() {
-        RespondToApplicationRequest request = RespondToApplicationRequest.builder()
-            .response(TseRespondType.builder().build())
-            .build();
+    TestData data = new TestData();
 
+    @Test
+    @DisplayName("given resp app, "
+        + "when setting claimant response "
+        + "then status is waitingForTheTribunal")
+    void respAppStatusAfterClaimantResponse() {
+        RespondToApplicationRequest request = data.getRespondToApplicationRequest();
         GenericTseApplicationType app = GenericTseApplicationType.builder().build();
-        CaseData caseData = new CaseData();
+        CaseData caseData = data.getCaseData();
         CaseDocumentService service = mock(CaseDocumentService.class);
 
         TseApplicationHelper.setRespondentApplicationWithResponse(request, app, caseData, service);
 
-        assertEquals("waitingForTheTribunal", app.getApplicationState());
+        Assertions.assertEquals("waitingForTheTribunal", app.getApplicationState());
     }
 }
