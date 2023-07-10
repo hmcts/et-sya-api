9584d6b8621c6af666b3f40651057b277b772579
mehmet.dede <mehmet.dede@justice.gov.uk>
2023-07-10 15:29:42 +0100

    Pact Test Updates

--- src/contractTest/java/uk/gov/hmcts/reform/et/syaapi/consumer/ccd/CcdGetCasesByCaseIdPactTest.java
+++ src/contractTest/java/uk/gov/hmcts/reform/et/syaapi/consumer/ccd/CcdGetCasesByCaseIdPactTest.java
@@ -11,7 +11,7 @@ import uk.gov.hmcts.reform.et.syaapi.consumer.SpringBootContractBaseTest;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.springframework.http.HttpMethod.POST;
+import static org.springframework.http.HttpMethod.GET;
 import static org.springframework.http.HttpStatus.OK;
 
 class CcdGetCasesByCaseIdPactTest extends SpringBootContractBaseTest {
@@ -22,9 +22,8 @@ class CcdGetCasesByCaseIdPactTest extends SpringBootContractBaseTest {
         return builder
             .given("a case exists")
             .uponReceiving("Provider receives a GET /cases/{caseId} request from et-sya-api API")
-            .path("/cases/user-case/")
-            .method(POST.toString())
-            .body("{\"case_id\": \"1668618837188374\"}")
+            .path("/cases/1668618837188374/")
+            .method(GET.toString())
             .willRespondWith()
             .status(OK.value())
             .headers(RESPONSE_HEADERS)
@@ -35,7 +34,8 @@ class CcdGetCasesByCaseIdPactTest extends SpringBootContractBaseTest {
     @Test
     @PactTestFor(pactMethod = "executeCcdGetCasesByCaseId")
     void verifyGetCaseById() {
-        CaseDetails caseDetails = coreCaseDataApi.getCase(SERVICE_AUTH_TOKEN, AUTH_TOKEN, String.valueOf(CASE_ID));
+        CaseDetails caseDetails = coreCaseDataApi.getCase(SERVICE_AUTH_TOKEN, AUTH_TOKEN,
+                                                          String.valueOf("1668618837188374"));
 
         assertThat(caseDetails.getSecurityClassification().name(), is(PUBLIC));
         assertThat(caseDetails.getJurisdiction(), is(JURISDICTION_ID));
@@ -43,7 +43,7 @@ class CcdGetCasesByCaseIdPactTest extends SpringBootContractBaseTest {
 
     private PactDslJsonBody createCasesResponse() {
         return new PactDslJsonBody()
-            .stringType("id", String.valueOf(CASE_ID))
+            .stringType("id", String.valueOf("1668618837188374"))
             .stringValue("jurisdiction", JURISDICTION_ID)
             .stringValue("case_type", CASE_TYPE_ID)
             .stringValue("security_classification", PUBLIC);
