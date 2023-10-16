200914de204b79906ab17630597936735e5c4510
Cindy Chan <94921454+cindychmcts@users.noreply.github.com>
2023-10-16 14:41:59 +0100

    TseApplicationHelper

--- src/main/java/uk/gov/hmcts/reform/et/syaapi/helper/TseApplicationHelper.java
+++ src/main/java/uk/gov/hmcts/reform/et/syaapi/helper/TseApplicationHelper.java
@@ -23,7 +23,6 @@ import static uk.gov.hmcts.ecm.common.model.helper.Constants.STORED;
 import static uk.gov.hmcts.ecm.common.model.helper.Constants.STORED_STATE;
 import static uk.gov.hmcts.reform.et.syaapi.constants.EtSyaConstants.CLAIMANT_CORRESPONDENCE_DOCUMENT;
 import static uk.gov.hmcts.reform.et.syaapi.constants.EtSyaConstants.UK_LOCAL_DATE_PATTERN;
-import static uk.gov.hmcts.reform.et.syaapi.constants.EtSyaConstants.YES;
 
 @Slf4j
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
