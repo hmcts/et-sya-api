4c0d4a051e05857d719b886c4e9b6072ac17557a
marcjudge <98343606+marcjudge@users.noreply.github.com>
2023-10-25 17:40:54 +0100

    Sonar code smells (#596)
    * Fix code smells
    
    * Format

--- src/main/java/uk/gov/hmcts/reform/et/syaapi/service/CaseService.java
+++ src/main/java/uk/gov/hmcts/reform/et/syaapi/service/CaseService.java
@@ -95,9 +95,9 @@ public class CaseService {
     private static final String ALL_CASES_QUERY = "{\"size\":10000,\"query\":{\"match_all\": {}}}";
 
     @Value("${caseWorkerUserName}")
-    private transient String caseWorkerUserName;
+    private String caseWorkerUserName;
     @Value("${caseWorkerPassword}")
-    private transient String caseWorkerPassword;
+    private String caseWorkerPassword;
 
     /**
      * Given a case id in the case request, this will retrieve the correct {@link CaseDetails}.
