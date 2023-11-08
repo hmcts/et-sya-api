30865fb564759046a01f3e61488c3b60df92bcff
Cindy Chan <94921454+cindychmcts@users.noreply.github.com>
2023-11-08 17:04:10 +0000

    PdfServiceTest

--- src/test/java/uk/gov/hmcts/reform/et/syaapi/service/pdf/PdfServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/et/syaapi/service/pdf/PdfServiceTest.java
@@ -43,7 +43,6 @@ import java.util.concurrent.ConcurrentHashMap;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.atLeast;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.when;
