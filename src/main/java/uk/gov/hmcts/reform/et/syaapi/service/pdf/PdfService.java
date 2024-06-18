acfe757ad8fd87f63d81271829a69d5190b44b29
mehmet.dede <mehmet.dede@justice.gov.uk>
2024-06-18 17:03:09 +0100

    Update to correct character codes.

--- src/main/java/uk/gov/hmcts/reform/et/syaapi/service/pdf/PdfService.java
+++ src/main/java/uk/gov/hmcts/reform/et/syaapi/service/pdf/PdfService.java
@@ -111,11 +111,11 @@ public class PdfService {
         if (!ObjectUtils.isEmpty(stream)) {
             try (PDDocument pdfDocument = Loader.loadPDF(
                 Objects.requireNonNull(stream))) {
-                PDDocumentCatalog pdDocumentCatalog = pdfDocument.getDocumentCatalog();
                 PDResources resources = new PDResources();
                 resources.put(COSName.getPDFName(TIMES_NEW_ROMAN_PDFBOX_CHARACTER_CODE), PDType1Font.TIMES_ROMAN);
                 resources.put(COSName.getPDFName(HELVETICA_PDFBOX_CHARACTER_CODE_1), PDType1Font.HELVETICA);
                 resources.put(COSName.getPDFName(HELVETICA_PDFBOX_CHARACTER_CODE_2), PDType1Font.HELVETICA);
+                PDDocumentCatalog pdDocumentCatalog = pdfDocument.getDocumentCatalog();
                 PDAcroForm pdfForm = pdDocumentCatalog.getAcroForm();
                 pdfForm.setDefaultResources(resources);
                 for (Map.Entry<String, Optional<String>> entry : this.pdfMapperService.mapHeadersToPdf(caseData)
