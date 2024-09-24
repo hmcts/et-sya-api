3583e086d605f59e26e9fed1d6c843336a5494f0
mehmet.dede <mehmet.dede@justice.gov.uk>
2024-09-24 16:22:53 +0100

    Elastic search query updated with a string value

--- src/main/java/uk/gov/hmcts/reform/et/syaapi/service/ManageCaseRoleService.java
+++ src/main/java/uk/gov/hmcts/reform/et/syaapi/service/ManageCaseRoleService.java
@@ -145,6 +145,7 @@ public class ManageCaseRoleService {
             HttpEntity<CaseAssignmentUserRolesRequest> requestEntity =
                 new HttpEntity<>(caseAssignmentUserRolesRequest,
                                  RemoteServiceUtil.buildHeaders(adminToken, this.authTokenGenerator.generate()));
+            log.info(ccdApiUrl);
             response = restTemplate.exchange(ccdApiUrl + ManageCaseRoleConstants.CASE_USERS_API_URL,
                                              httpMethod,
                                              requestEntity,
