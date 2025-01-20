49e65fc82a875390d4476af79746de70ad1fb1de
btensay <tensay.bulcha@gmail.com>
2025-01-20 10:26:00 +0000

    ttl value removed upon case submission

--- src/main/java/uk/gov/hmcts/reform/et/syaapi/service/ApplicationService.java
+++ src/main/java/uk/gov/hmcts/reform/et/syaapi/service/ApplicationService.java
@@ -88,6 +88,8 @@ public class ApplicationService {
         CaseDetails caseDetails = startEventResponse.getCaseDetails();
         ClaimantTse claimantTse = request.getClaimantTse();
         caseDetails.getData().put("claimantTse", claimantTse);
+        //Remove case time to live to avoid case being deleted
+        caseDetails.getData().remove("TTL");
 
         try {
             log.info("Uploading pdf of TSE application");
