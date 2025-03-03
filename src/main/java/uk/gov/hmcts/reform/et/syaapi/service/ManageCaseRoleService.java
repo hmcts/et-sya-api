89a264dd79025780912b4b6e52fb6075fe6385c1
btensay <tensay.bulcha@gmail.com>
2025-03-03 13:48:59 +0000

    pmd fixed

--- src/main/java/uk/gov/hmcts/reform/et/syaapi/service/ManageCaseRoleService.java
+++ src/main/java/uk/gov/hmcts/reform/et/syaapi/service/ManageCaseRoleService.java
@@ -202,13 +202,11 @@ public class ManageCaseRoleService {
 
     private List<CaseDetails> updateAllRespondentsIdamIdAndDefaultLinkStatuses(
         String authorisation, ModifyCaseUserRolesRequest modifyCaseUserRolesRequest, String modificationType) {
-        List<CaseDetails> caseDetailsList = new ArrayList<>();
-
         log.info("updateAllRespondentsIdamIdAndDefaultLinkStatuses\n");
         log.info("modifyCaseUserRolesRequest: \n" + modifyCaseUserRolesRequest.toString());
         log.info("getModifyCaseUserRoles: \n" + modifyCaseUserRolesRequest.getModifyCaseUserRoles().toString());
         log.info("modificationType: \n" + modificationType);
-
+        List<CaseDetails> caseDetailsList = new ArrayList<>();
         for (ModifyCaseUserRole modifyCaseUserRole : modifyCaseUserRolesRequest.getModifyCaseUserRoles()) {
             if (ManageCaseRoleConstants.CASE_USER_ROLE_DEFENDANT.equals(modifyCaseUserRole.getCaseRole())) {
                 CaseDetails caseDetails =
