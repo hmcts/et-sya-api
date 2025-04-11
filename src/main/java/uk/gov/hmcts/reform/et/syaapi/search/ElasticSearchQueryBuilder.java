ba6fe3d7eac8e184b6c258963a6e0a4ca69ccba8
Harpreet Jhita <harpreet.jhita@justice.gov.uk>
2025-04-11 15:26:08 +0100

    Updates for checkstyle

--- src/main/java/uk/gov/hmcts/reform/et/syaapi/search/ElasticSearchQueryBuilder.java
+++ src/main/java/uk/gov/hmcts/reform/et/syaapi/search/ElasticSearchQueryBuilder.java
@@ -178,6 +178,7 @@ public final class ElasticSearchQueryBuilder {
                   ]
                 }
               }
-            }""".formatted(ethosCaseReference);
+            }
+            """.formatted(ethosCaseReference);
     }
 }
--- src/test/java/uk/gov/hmcts/reform/et/syaapi/service/utils/TestConstants.java
+++ src/test/java/uk/gov/hmcts/reform/et/syaapi/service/utils/TestConstants.java
@@ -117,31 +117,32 @@ public final class TestConstants {
     public static final String TEST_JURISDICTION_ID_EMPLOYMENT = "EMPLOYMENT";
     public static final String TEST_ETHOS_CASE_REFERENCE = "6000032/2024";
     public static final String EXPECTED_QUERY_BY_ETHOS_CASE_REFERENCE = """
-    {
-      "size": 1,
-      "query": {
-        "bool": {
-          "must": [
-            {
-              "match": {
-                "data.ethosCaseReference.keyword": {
-                  "query": "6000032/2024"
+        {
+          "size": 1,
+          "query": {
+            "bool": {
+              "must": [
+                {
+                  "match": {
+                    "data.ethosCaseReference.keyword": {
+                      "query": "6000032/2024"
+                    }
+                  }
                 }
-              }
-            }
-          ],
-          "must_not": [
-            {
-              "match": {
-                "data.migratedFromEcm": {
-                  "query": "Yes"
+              ],
+              "must_not": [
+                {
+                  "match": {
+                    "data.migratedFromEcm": {
+                      "query": "Yes"
+                    }
+                  }
                 }
-              }
+              ]
             }
-          ]
+          }
         }
-      }
-    }""";
+        """;
     public static final String EXPECTED_QUERY_BY_ID = "{\"size\":1,\"query\":{\"bool\":{\"must\":[{\"match\":"
         + "{\"reference.keyword\":{\"query\":\"1234567890123456\"}}},{\"match\":{\"state.keyword\":{\"query\""
                                                       + ":\"Accepted\"}}}],\"boost\":1.0}}}";
