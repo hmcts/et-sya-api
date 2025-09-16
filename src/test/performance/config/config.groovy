9d8a47966fa07228c12966d2fefa43d014b21d16
Dean Cullen <Dean_cullen@next.co.uk>
2025-09-16 11:07:50 +0100

    Removed unused code in config

--- src/test/performance/config/config.groovy
+++ src/test/performance/config/config.groovy
@@ -1,15 +1,15 @@
 // ====== Set up default values ======
-this.dynatraceApiHost = "https://yrk32651.live.dynatrace.com/"
-this.dynatraceEventIngestEndpoint = "api/v2/events/ingest"
-this.dynatraceMetricIngestEndpoint = "api/v2/metrics/ingest"
-this.dynatraceTriggerSyntheticEndpoint = "api/v2/synthetic/executions/batch"
-this.dynatraceUpdateSyntheticEndpoint = "api/v1/synthetic/monitors/"
+//this.dynatraceApiHost = "https://yrk32651.live.dynatrace.com/"
+//this.dynatraceEventIngestEndpoint = "api/v2/events/ingest"
+//this.dynatraceMetricIngestEndpoint = "api/v2/metrics/ingest"
+//this.dynatraceTriggerSyntheticEndpoint = "api/v2/synthetic/executions/batch"
+//this.dynatraceUpdateSyntheticEndpoint = "api/v1/synthetic/monitors/"
 this.dynatraceMetricType = 'et-sya-api'
 this.dynatraceMetricTag = 'namespace:et'
-this.dynatraceSyntheticTest = ""
-this.dynatraceDashboardId = ""
-this.dynatraceDashboardURL = ""
-this.dynatraceEntitySelector = " "
+//this.dynatraceSyntheticTest = ""
+//this.dynatraceDashboardId = ""
+//this.dynatraceDashboardURL = ""
+//this.dynatraceEntitySelector = " "
 //Preview Config
 this.dynatraceSyntheticTestPreview = "HTTP_CHECK-467146A25406C026"
 this.dynatraceDashboardIdPreview = "f44ff1f7-e13a-4a6a-95b9-4e3ab44e6587"
@@ -25,8 +25,6 @@ this.dynatraceSyntheticTest = "HTTP_CHECK-0A7CF661E2924918"
 this.dynatraceDashboardIdPerfTest = "a4576442-06a9-4a76-baa5-5342a525679f"
 this.dynatraceDashboardURLPerfTest = "https://yrk32651.live.dynatrace.com/#dashboard;id=a4576442-06a9-4a76-baa5-5342a525679f;applyDashboardDefaults=true"
 this.dynatraceEntitySelectorPerfTest = 'type(service),tag(\\"[Kubernetes]namespace:et\\"),tag(\\"Environment:PERF\\"),entityId(\\"SERVICE-894163B308FBDD78\\")'
-
-this.Test = "CheckingConfigFile"
 
 echo "Completed Config Load..." 
 
