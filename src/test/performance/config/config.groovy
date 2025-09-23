4ef1e4e7616675f544afb4e80fe3bd25f7543675
Dean Cullen <Dean_cullen@next.co.uk>
2025-09-23 11:44:22 +0100

    Updated preview config

--- src/test/performance/config/config.groovy
+++ src/test/performance/config/config.groovy
@@ -1,20 +1,11 @@
 // ====== Set up default values ======
-//this.dynatraceApiHost = "https://yrk32651.live.dynatrace.com/"
-//this.dynatraceEventIngestEndpoint = "api/v2/events/ingest"
-//this.dynatraceMetricIngestEndpoint = "api/v2/metrics/ingest"
-//this.dynatraceTriggerSyntheticEndpoint = "api/v2/synthetic/executions/batch"
-//this.dynatraceUpdateSyntheticEndpoint = "api/v1/synthetic/monitors/"
 this.dynatraceMetricType = 'et-sya-api'
 this.dynatraceMetricTag = 'namespace:et'
-//this.dynatraceSyntheticTest = ""
-//this.dynatraceDashboardId = ""
-//this.dynatraceDashboardURL = ""
-//this.dynatraceEntitySelector = " "
 //Preview Config
 this.dynatraceSyntheticTestPreview = "HTTP_CHECK-467146A25406C026"
 this.dynatraceDashboardIdPreview = "f44ff1f7-e13a-4a6a-95b9-4e3ab44e6587"
 this.dynatraceDashboardURLPreview = "https://yrk32651.live.dynatrace.com/#dashboard;id=f44ff1f7-e13a-4a6a-95b9-4e3ab44e6587;applyDashboardDefaults=true"
-this.dynatraceEntitySelectorPreview = 'type(service),tag(\\"[Kubernetes]namespace:et\\"),tag(\\"Environment:Preview\\"),entityId(\\"SERVICE-94E67F588E12D9D6\\")' //SERVICE-85A65FBFF3C9F37E
+this.dynatraceEntitySelectorPreview = 'type(service),tag(\\"[Kubernetes]namespace:et\\"),tag(\\"Environment:Preview\\"),entityId(\\"SERVICE-A1223BBF43F01226\\")' //SERVICE-85A65FBFF3C9F37E //SERVICE-94E67F588E12D9D6
 //AAT Config
 this.dynatraceSyntheticTestAAT = "HTTP_CHECK-8BD69A31834A24FC"
 this.dynatraceDashboardIdAAT = "a529a685-8c36-4e8c-8137-67de8bfcf104"
