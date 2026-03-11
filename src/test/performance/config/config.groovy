0f0dece9d879974bab5304f0d27614d011b35f7a
Dean Cullen <Dean_cullen@next.co.uk>
2026-03-11 12:24:06 +0000

    Updated all env entity selectors

--- src/test/performance/config/config.groovy
+++ src/test/performance/config/config.groovy
@@ -12,13 +12,15 @@ this.dynatraceEntitySelectorPreview = 'type(service),tag(\\"[Kubernetes]namespac
 this.dynatraceSyntheticTestAAT = "HTTP_CHECK-8BD69A31834A24FC"
 this.dynatraceDashboardIdAAT = "a529a685-8c36-4e8c-8137-67de8bfcf104"
 this.dynatraceDashboardURLAAT = "https://yrk32651.live.dynatrace.com/#dashboard;gtf=-2h;gf=all;id=a529a685-8c36-4e8c-8137-67de8bfcf104"
-this.dynatraceEntitySelectorAAT = 'type(service),tag(\\"[Kubernetes]namespace:et\\"),tag(\\"Environment:AAT\\"),entityId(\\"SERVICE-A930A031C9C0359E\\")'
+this.dynatraceEntitySelectorAAT = 'type(service),tag(\\"[Kubernetes]namespace:et\\"),tag(\\"Environment:AAT\\"),entityName.equals(\\"ManageCaseController (AAT)\\")'
+//this.dynatraceEntitySelectorAAT = 'type(service),tag(\\"[Kubernetes]namespace:et\\"),tag(\\"Environment:AAT\\"),entityId(\\"SERVICE-A930A031C9C0359E\\")'//
 
 //Perftest Config
 this.dynatraceSyntheticTestPerfTest = "HTTP_CHECK-0A7CF661E2924918"
 this.dynatraceDashboardIdPerfTest = "a4576442-06a9-4a76-baa5-5342a525679f"
 this.dynatraceDashboardURLPerfTest = "https://yrk32651.live.dynatrace.com/#dashboard;id=a4576442-06a9-4a76-baa5-5342a525679f;applyDashboardDefaults=true"
-this.dynatraceEntitySelectorPerfTest = 'type(service),tag(\\"[Kubernetes]namespace:et\\"),tag(\\"Environment:PERF\\"),entityId(\\"SERVICE-894163B308FBDD78\\")'
+this.dynatraceEntitySelectorPerfTest = 'type(service),tag(\\"[Kubernetes]namespace:et\\"),tag(\\"Environment:PERF\\"),entityName.equals(\\"ManageCaseController (PERF)\\")'
+//this.dynatraceEntitySelectorPerfTest = 'type(service),tag(\\"[Kubernetes]namespace:et\\"),tag(\\"Environment:PERF\\"),entityId(\\"SERVICE-894163B308FBDD78\\")'
 
 echo "Completed Config Load..." 
 
